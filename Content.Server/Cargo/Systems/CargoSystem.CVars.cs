dc9eceb2be33ca47a2e05a54fa8e3010a5cad799
VMSolidus <evilexecutive@gmail.com>
2024-04-30 21:18:23 -0400

    No more double trade stations

---
+++ Content.Server/Cargo/Systems/CargoSystem.CVars.cs
@@ -0,0 +1,24 @@
+using Robust.Shared.Configuration;
+
+namespace Content.Server.Cargo.Systems
+{
+    public sealed partial class CargoSystem
+    {
+        [Dependency] private readonly IConfigurationManager _cfg = default!;
+    }
+
+    [CVarDefs]
+    public sealed class CargoCVars
+    {
+        /// <summary>
+        ///     Determines if a trade station spawns in its own FTL map.
+        /// </summary>
+        /// <remarks>
+        ///     Set this to true if and only if you are disabling the normal spawning method.
+        ///     Otherwise you get two trade stations.
+        ///     This does NOTHING to the trade station that spawns on the default map.
+        /// </remarks>
+        public static readonly CVarDef<bool> CreateCargoMap =
+            CVarDef.Create("cargo.tradestation_spawns_in_ftl_map", false, CVar.SERVERONLY);
+    }
+}
