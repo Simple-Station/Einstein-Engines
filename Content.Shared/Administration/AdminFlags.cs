9bb149f42185f9c23dc606334e5a69442963321f
SimpleStation14 <Unknown>
2024-05-03 11:55:08 -0400

    Cherry-picked commit dca0c6694bce56fcf962feab2b59ad4a849f4533 from space-wizards/space-station-14/master

--- Content.Shared/Administration/AdminFlags.cs
+++ Content.Shared/Administration/AdminFlags.cs
@@ -96,9 +96,9 @@
         MassBan = 1 << 15,
 
         /// <summary>
-        ///     DeltaV - The ability to whitelist people. Either this permission or +BAN is required for remove.
+        /// Allows you to remain hidden from adminwho except to other admins with this flag.
         /// </summary>
-        Whitelist = 1 << 20,
+        Stealth = 1 << 16,
 
         /// <summary>
         ///     Dangerous host permissions like scsi.
