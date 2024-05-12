Simple-Station/Einstein-Engines

commit 0947a523f926d3e7926c2e39742013d3b84340b5

Revert Salvage Shittle and Trading Post removal (#344)
It's been gone for a bit too long.

Resources/Prototypes/Entities/Stations/base.yml
@@ -1,4 +1,4 @@
-﻿- type: entity
+- type: entity
   id: BaseStation
   abstract: true
   components:
@@ -46,15 +46,15 @@
       path: /Maps/Shuttles/cargo.yml
     - type: GridSpawn
       groups:
-#        trade:
-#          addComponents:
-#          - type: ProtectedGrid
-#          - type: TradeStation
-#          paths:
-#          - /Maps/Shuttles/trading_outpost.yml
-#        mining:
-#          paths:
-#          - /Maps/Shuttles/mining.yml
+        trade:
+          addComponents:
+          - type: ProtectedGrid
+          - type: TradeStation
+          paths:
+          - /Maps/Shuttles/trading_outpost.yml
+        mining:
+          paths:
+          - /Maps/Shuttles/mining.yml
         # Spawn last
         ruins:
           hide: true
@@ -80,9 +80,9 @@
         cargo:
           paths:
           - /Maps/Shuttles/cargo_core.yml
-#        mining:
-#          paths:
-#          - /Maps/Shuttles/mining.yml
+        mining:
+          paths:
+          - /Maps/Shuttles/mining.yml
         ruins:
           hide: true
           nameGrid: true

commit 42145993f831f7c3da0260c84431b02a4e5affde

Fix Double Trade Station Bug (#351)
# Description

This fixes a bug introduced inadvertently by
https://github.com/Simple-Station/Einstein-Engines/pull/344 by adding a
CVar that makes it a choice to be made by our downstreams. Also, welcome
to our new CVar standard where they should be included in other systems.

---------

Co-authored-by: Danger Revolution! <142105406+DangerRevolution@users.noreply.github.com>

Content.Server/Cargo/Systems/CargoSystem.CVars.cs
@@ -1,0 +1,26 @@
+using Robust.Shared.Configuration;
+
+namespace Content.Server.Cargo.Systems
+{
+    public sealed partial class CargoSystem
+    {
+        [Dependency] private readonly IConfigurationManager _cfg = default!;
+    }
+
+    [CVarDefs]
+    public sealed class CargoCVars
+    {
+        /// <summary>
+        ///     Determines if a trade station spawns in its own FTL map.
+        /// </summary>
+        /// <remarks>
+        ///     Set this to true if and only if you are disabling the normal spawning method.
+        ///     Otherwise you get two trade stations.
+        ///     This does NOTHING to the trade station that spawns on the default map.
+        ///     To change that, in Resources/Prototypes/Entities/Stations/base.yml;
+        ///      comment out the "trade:" section.
+        /// </remarks>
+        public static readonly CVarDef<bool> CreateCargoMap =
+            CVarDef.Create("cargo.tradestation_spawns_in_ftl_map", false, CVar.SERVERONLY);
+    }
+}

Content.Server/Cargo/Systems/CargoSystem.Shuttle.cs
@@ -19,6 +19,7 @@ using Robust.Shared.Random;
 using Robust.Shared.Audio;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Utility;
+using Robust.Shared.Configuration;
 
 namespace Content.Server.Cargo.Systems;
 
@@ -27,7 +28,7 @@ public sealed partial class CargoSystem
     /*
      * Handles cargo shuttle / trade mechanics.
      */
-
+    [Dependency] private readonly IConfigurationManager _confMan = default!;
     public MapId? CargoMap { get; private set; }
 
     private static readonly SoundPathSpecifier ApproveSound = new("/Audio/Effects/Cargo/ping.ogg");
@@ -369,7 +370,7 @@ public sealed partial class CargoSystem
         if (!HasComp<StationCargoOrderDatabaseComponent>(args.Station)) // No cargo, L
             return;
 
-        if (_cfgManager.GetCVar(CCVars.GridFill))
+        if (_cfgManager.GetCVar(CCVars.GridFill) && _confMan.GetCVar(CargoCVars.CreateCargoMap))
             SetupTradePost();
     }
 

Resources/Prototypes/Entities/Stations/base.yml
@@ -49,6 +49,7 @@
         trade:
           addComponents:
           - type: ProtectedGrid
+          # If you comment out the tradestation, make sure to also set cargo.tradestation_spawns_in_ftl_map to TRUE
           - type: TradeStation
           paths:
           - /Maps/Shuttles/trading_outpost.yml