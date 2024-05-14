8a265c875412785c1754899d37b61acebea256b0
beck-thompson <107373427+beck-thompson@users.noreply.github.com>
2024-05-14 12:01:12 -0700

    Fix incorrect message displaying when trying to remove stuck item from someones hand. (#28024)
    * Fix
    
    * Fixed the other spot!
    
    (cherry picked from commit 550a3afc52035e19e659b01624956c6751d290e1)

--- Content.Server/Strip/StrippableSystem.cs
+++ Content.Server/Strip/StrippableSystem.cs
@@ -503,7 +503,7 @@ namespace Content.Server.Strip
 
             if (!_handsSystem.TryGetHand(target, handName, out var handSlot, target.Comp))
             {
-                _popupSystem.PopupCursor(Loc.GetString("strippable-component-item-slot-free-message", ("owner", target)), user);
+                _popupSystem.PopupCursor(Loc.GetString("strippable-component-item-slot-free-message", ("owner", Identity.Name(target, EntityManager, user))), user);
                 return false;
             }
 
@@ -518,7 +518,7 @@ namespace Content.Server.Strip
 
             if (!_handsSystem.CanDropHeld(target, handSlot, false))
             {
-                _popupSystem.PopupCursor(Loc.GetString("strippable-component-cannot-drop-message", ("owner", target)), user);
+                _popupSystem.PopupCursor(Loc.GetString("strippable-component-cannot-drop-message", ("owner", Identity.Name(target, EntityManager, user))), user);
                 return false;
             }
 
