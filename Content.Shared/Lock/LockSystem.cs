3eb106ea13075af9131872ff9135484d939acb73
deltanedas <39013340+deltanedas@users.noreply.github.com>
2024-05-29 20:08:33 +0000

    item cabinet rework + stuff (#26779)
    (cherry picked from commit 404fe818e8dffc9166c8572a4725283a5b88a5cd)

--- Content.Shared/Lock/LockSystem.cs
+++ Content.Shared/Lock/LockSystem.cs
@@ -246,6 +246,7 @@ public sealed class LockSystem : EntitySystem
         return !userEv.Cancelled;
     }
 
+    // TODO: this should be a helper on AccessReaderSystem since so many systems copy paste it
     private bool HasUserAccess(EntityUid uid, EntityUid user, AccessReaderComponent? reader = null, bool quiet = true)
     {
         // Not having an AccessComponent means you get free access. woo!
