c4fa888edd9d477b09357f98946c26fc12032155
deltanedas <39013340+deltanedas@users.noreply.github.com>
2024-06-13 16:40:41 +0000

    listening post rewrite (#1369)
    * rename pirate radio map to listening post, change spawner prototype
    
    * refactor pirate radio spawn rule into DebrisSpawner and LoadFarGrid
    
    * remove obsolete yml
    
    * -m make listening post rule use new stuff and antag selection
    
    * other changes
    
    * fixes
    
    * more
    
    * fixy
    
    * fix nan
    
    * final fix
    
    * what
    
    ---------
    
    Co-authored-by: deltanedas <@deltanedas:kde.org>

--- Content.Server/Roles/RoleSystem.cs
+++ Content.Server/Roles/RoleSystem.cs
@@ -18,6 +18,7 @@ public sealed class RoleSystem : SharedRoleSystem
         SubscribeAntagEvents<TraitorRoleComponent>();
         SubscribeAntagEvents<ZombieRoleComponent>();
         SubscribeAntagEvents<ThiefRoleComponent>();
+        SubscribeAntagEvents<ListeningPostRoleComponent>(); // DeltaV - listening post role
     }
 
     public string? MindGetBriefing(EntityUid? mindId)
