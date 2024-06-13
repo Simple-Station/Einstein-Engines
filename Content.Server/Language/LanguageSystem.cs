45c1bf0efd8e418b5d467fb3409b98edf4f27bdd
fox <daytimer253@gmail.com>
2024-06-14 02:59:25 +0300

    Discover polymorphism

--- Content.Server/Language/LanguageSystem.cs
+++ Content.Server/Language/LanguageSystem.cs
@@ -17,10 +17,6 @@ public sealed partial class LanguageSystem : SharedLanguageSystem
         _determineLanguagesEvent = new(string.Empty, new(), new()),
         _universalLanguagesEvent = new(UniversalPrototype, [UniversalPrototype], [UniversalPrototype]); // Returned for universal speakers only
 
-    /// <summary>
-    ///   A random number added to each pseudo-random number's seed. Changes every round.
-    /// </summary>
-    public int RandomRoundSeed { get; private set; }
 
 
     public override void Initialize()
@@ -29,7 +25,6 @@ public sealed partial class LanguageSystem : SharedLanguageSystem
 
         SubscribeNetworkEvent<LanguagesSetMessage>(OnClientSetLanguage);
         SubscribeLocalEvent<LanguageSpeakerComponent, ComponentInit>(OnInitLanguageSpeaker);
-        SubscribeLocalEvent<RoundStartingEvent>(_ => RandomRoundSeed = _random.Next());
 
         InitializeNet();
     }
@@ -41,22 +36,8 @@ public sealed partial class LanguageSystem : SharedLanguageSystem
     /// </summary>
     public string ObfuscateSpeech(EntityUid source, string message)
     {
-        var language = GetLanguage(source) ?? Universal;
+        var language = GetLanguage(source);
         return ObfuscateSpeech(message, language);
-    }
-
-    /// <summary>
-    ///     Obfuscate a message using the given language.
-    /// </summary>
-    public string ObfuscateSpeech(string message, LanguagePrototype language)
-    {
-        var builder = new StringBuilder();
-        if (language.ObfuscateSyllables)
-            ObfuscateSyllables(builder, message, language);
-        else
-            ObfuscatePhrases(builder, message, language);
-
-        return builder.ToString();
     }
 
     public bool CanUnderstand(EntityUid listener, LanguagePrototype language, LanguageSpeakerComponent? listenerLanguageComp = null)
@@ -156,76 +137,6 @@ public sealed partial class LanguageSystem : SharedLanguageSystem
     }
     #endregion
 
-    #region internal api - obfuscation
-    private void ObfuscateSyllables(StringBuilder builder, string message, LanguagePrototype language)
-    {
-        // Go through each word. Calculate its hash sum and count the number of letters.
-        // Replicate it with pseudo-random syllables of pseudo-random (but similar) length. Use the hash code as the seed.
-        // This means that identical words will be obfuscated identically. Simple words like "hello" or "yes" in different langs can be memorized.
-        var wordBeginIndex = 0;
-        var hashCode = 0;
-        for (var i = 0; i < message.Length; i++)
-        {
-            var ch = char.ToLower(message[i]);
-            // A word ends when one of the following is found: a space, a sentence end, or EOM
-            if (char.IsWhiteSpace(ch) || IsSentenceEnd(ch) || i == message.Length - 1)
-            {
-                var wordLength = i - wordBeginIndex;
-                if (wordLength > 0)
-                {
-                    var newWordLength = PseudoRandomNumber(hashCode, 1, 4);
-
-                    for (var j = 0; j < newWordLength; j++)
-                    {
-                        var index = PseudoRandomNumber(hashCode + j, 0, language.Replacement.Count);
-                        builder.Append(language.Replacement[index]);
-                    }
-                }
-
-                builder.Append(ch);
-                hashCode = 0;
-                wordBeginIndex = i + 1;
-            }
-            else
-                hashCode = hashCode * 31 + ch;
-        }
-    }
-
-    private void ObfuscatePhrases(StringBuilder builder, string message, LanguagePrototype language)
-    {
-        // In a similar manner, each phrase is obfuscated with a random number of conjoined obfuscation phrases.
-        // However, the number of phrases depends on the number of characters in the original phrase.
-        var sentenceBeginIndex = 0;
-        for (var i = 0; i < message.Length; i++)
-        {
-            var ch = char.ToLower(message[i]);
-            if (IsSentenceEnd(ch) || i == message.Length - 1)
-            {
-                var length = i - sentenceBeginIndex;
-                if (length > 0)
-                {
-                    var newLength = (int) Math.Clamp(Math.Cbrt(length) - 1, 1, 4); // 27+ chars for 2 phrases, 64+ for 3, 125+ for 4.
-
-                    for (var j = 0; j < newLength; j++)
-                    {
-                        var phrase = _random.Pick(language.Replacement);
-                        builder.Append(phrase);
-                    }
-                }
-                sentenceBeginIndex = i + 1;
-
-                if (IsSentenceEnd(ch))
-                    builder.Append(ch).Append(" ");
-            }
-        }
-    }
-
-    private static bool IsSentenceEnd(char ch)
-    {
-        return ch is '.' or '!' or '?';
-    }
-    #endregion
-
     #region internal api - misc
     /// <summary>
     ///   Dynamically resolves the current language of the entity and the list of all languages it speaks.
@@ -255,19 +166,6 @@ public sealed partial class LanguageSystem : SharedLanguageSystem
         if (ev.CurrentLanguage.Length == 0)
             ev.CurrentLanguage = !string.IsNullOrEmpty(comp.CurrentLanguage) ? comp.CurrentLanguage : UniversalPrototype; // Fall back to account for admemes like admins possessing a bread
         return ev;
-    }
-
-    /// <summary>
-    ///   Generates a stable pseudo-random number in the range (min, max) for the given seed.
-    ///   Each input seed corresponds to exactly one random number.
-    /// </summary>
-    private int PseudoRandomNumber(int seed, int min, int max)
-    {
-        // This is not a uniform distribution, but it shouldn't matter given there's 2^31 possible random numbers,
-        //   the bias of this function should be so tiny it will never be noticed.
-        seed += RandomRoundSeed;
-        var random = ((seed * 1103515245) + 12345) & 0x7fffffff; // Source: http://cs.uccs.edu/~cs591/bufferOverflow/glibc-2.2.4/stdlib/random_r.c
-        return random % (max - min) + min;
     }
 
     /// <summary>
--- Content.Shared/Language/LanguagePrototype.cs
+++ Content.Shared/Language/LanguagePrototype.cs
@@ -10,15 +10,13 @@ public sealed class LanguagePrototype : IPrototype
     public string ID { get; private set;  } = default!;
 
     /// <summary>
-    ///     If true, obfuscated phrases of creatures speaking this language will have their syllables replaced with "replacement" syllables.
-    ///     Otherwise entire sentences will be replaced.
+    ///     Obfuscation method used by this language. By default, uses <see cref="ObfuscationMethod.Default"/>
     /// </summary>
-    [DataField(required: true)]
-    public bool ObfuscateSyllables;
+    [DataField("obfuscation")]
+    public ObfuscationMethod Obfuscation = ObfuscationMethod.Default;
 
     /// <summary>
-    ///     Lists all syllables that are used to obfuscate a message a listener cannot understand if obfuscateSyllables is true.
-    ///     Otherwise uses all possible phrases the creature can make when trying to say anything.
+    ///     A list of replacement phrases used in <see cref="ObfuscationMethod"/>
     /// </summary>
     [DataField(required: true)]
     public List<string> Replacement = [];
