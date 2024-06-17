bc93199eb79d6d893f62cd11323d8a454b447dbd
VMSolidus <evilexecutive@gmail.com>
2024-06-17 17:42:12 -0400

    Port Station Goals (#465)
    # Description
    
    https://github.com/Simple-Station/Parkstation-Friendly-Chainsaw/pull/10
    
    This adds a feature whereby a random goal for the shift is faxed to the
    station's Captain at the start of every shift.
    It is up to the Captain to decide if and how this goal is to be
    completed.
    Goals are randomly generated every shift, and are meant to help
    encourage station activity and RP.
    Admins are also able to send station goals via `sendstationgoal`.
    
    ---
    
    <details><summary><h1>Media</h1></summary>
    <p>
    
    
    ![stationgoals](https://github.com/Simple-Station/Einstein-Engines/assets/16548818/4a2b5533-dfee-4388-bfbc-043ee71b2647)
    
    ---
    
    </p>
    </details> 
    
    # Changelog
    
    :cl: VMSolidus
    - add: Added station goals that get sent to the Command fax machine at
    the start of every shift
    
    ---------
    
    Signed-off-by: VMSolidus <evilexecutive@gmail.com>
    Co-authored-by: Danger Revolution! <142105406+DangerRevolution@users.noreply.github.com>
    Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -2106,5 +2106,17 @@ namespace Content.Shared.CCVar
         /// </summary>
         public static readonly CVarDef<bool> PsionicRollsEnabled =
             CVarDef.Create("psionics.rolls_enabled", true, CVar.SERVERONLY);
+
+        /// <summary>
+        ///     Enables station goals
+        /// </summary>
+        public static readonly CVarDef<bool> StationGoalsEnabled =
+            CVarDef.Create("game.station_goals", true, CVar.SERVERONLY);
+
+        /// <summary>
+        ///     Chance for a station goal to be sent
+        /// </summary>
+        public static readonly CVarDef<float> StationGoalsChance =
+            CVarDef.Create("game.station_goals_chance", 0.1f, CVar.SERVERONLY);
     }
 }
