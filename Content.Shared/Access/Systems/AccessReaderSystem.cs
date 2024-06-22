f293d7e519ff55993729f3de46c978df54d1faa3
deltanedas <39013340+deltanedas@users.noreply.github.com>
2024-06-22 15:12:58 +0000

    add access reader log wire (#29094)
    * add LoggingDisabled to AccessReader
    
    * add LogWireAction
    
    * -m give everything besides high-security door a log wire
    
    * make LogAccess public and support string arg
    
    * add log when pulsing
    
    * m
    
    * l
    
    ---------
    
    Co-authored-by: deltanedas <@deltanedas:kde.org>

--- Content.Shared/Access/Systems/AccessReaderSystem.cs
+++ Content.Shared/Access/Systems/AccessReaderSystem.cs
@@ -380,11 +380,11 @@ public sealed class AccessReaderSystem : EntitySystem
     }
 
     /// <summary>
-    /// Logs an access
+    /// Logs an access for a specific entity.
     /// </summary>
     /// <param name="ent">The reader to log the access on</param>
     /// <param name="accessor">The accessor to log</param>
-    private void LogAccess(Entity<AccessReaderComponent> ent, EntityUid accessor)
+    public void LogAccess(Entity<AccessReaderComponent> ent, EntityUid accessor)
     {
         if (IsPaused(ent) || ent.Comp.LoggingDisabled)
             return;
@@ -428,7 +428,21 @@ public sealed class AccessReaderSystem : EntitySystem
             name = getIdentityShortInfoEvent.Title;
         }
 
-        name ??= Loc.GetString("access-reader-unknown-id");
+        LogAccess(ent, name ?? Loc.GetString("access-reader-unknown-id"));
+    }
+
+    /// <summary>
+    /// Logs an access with a predetermined name
+    /// </summary>
+    /// <param name="ent">The reader to log the access on</param>
+    /// <param name="name">The name to log as</param>
+    public void LogAccess(Entity<AccessReaderComponent> ent, string name)
+    {
+        if (IsPaused(ent) || ent.Comp.LoggingDisabled)
+            return;
+
+        if (ent.Comp.AccessLog.Count >= ent.Comp.AccessLogLimit)
+            ent.Comp.AccessLog.Dequeue();
 
         var stationTime = _gameTiming.CurTime.Subtract(_gameTicker.RoundStartTimeSpan);
         ent.Comp.AccessLog.Enqueue(new AccessRecord(stationTime, name));
