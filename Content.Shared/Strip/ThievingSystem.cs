98190e061d04a4abf5c3bc9e0c884c03a7316ab6
WarMechanic <69510347+WarMechanic@users.noreply.github.com>
2024-07-01 17:26:19 +1000

    Update Content.Shared/Strip/ThievingSystem.cs
    Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>
    Signed-off-by: WarMechanic <69510347+WarMechanic@users.noreply.github.com>

--- Content.Shared/Strip/ThievingSystem.cs
+++ Content.Shared/Strip/ThievingSystem.cs
@@ -43,17 +43,17 @@ public sealed class ThievingSystem : EntitySystem
 public enum ThievingStealth : sbyte
 {
     /// <summary>
-    /// Target sees a large popup indicating that an item is being stolen by who
+    /// 	Target sees a large popup indicating that an item is being stolen by who
     /// </summary>
     Obvious = 0,
 
     /// <summary>
-    /// Target sees a small popup indicating that an item is being stolen
+    /// 	Target sees a small popup indicating that an item is being stolen
     /// </summary>
     Subtle = 1,
 
     /// <summary>
-    /// Target does not see any popup regarding the stealing of an item
+    /// 	Target does not see any popup regarding the stealing of an item
     /// </summary>
     Hidden = 2
 }
