72b6e42941b6e5f6faff44611044fa2dbff7a0c8
deltanedas <39013340+deltanedas@users.noreply.github.com>
2024-07-01 20:05:56 +0000

    add glimmer wisp (#1383)
    * add sacrafice/drain sound
    
    * refactor glimmer event yml + uncomment wisp + make mites scale at lower glimmer
    
    * remove glimmer wisp rule and make GlimmerMobRule much more flexible + better
    
    * add LifeDrainer code that wisp uses
    
    * add ai primitives and compounds
    
    * add glimmer wisp
    
    * a
    
    * fixes
    
    * add wisp texture
    
    * desc
    
    * add sounds
    
    * buff so you cant literally 2 shot it with bible
    
    * make it harder to kill but drop more ectoplasm
    
    * make it work properly
    
    * let it use zap in combat i hope
    
    * add glimmer creatures guidebook
    
    * test
    
    ---------
    
    Co-authored-by: deltanedas <@deltanedas:kde.org>

--- Content.Server/DeltaV/StationEvents/Events/GlimmerMobSpawnRule.cs
+++ Content.Server/DeltaV/StationEvents/Events/GlimmerMobSpawnRule.cs
@@ -4,85 +4,75 @@ using Robust.Shared.Random;
 using Content.Server.GameTicking;
 using Content.Server.NPC.Components;
 using Content.Server.Psionics.Glimmer;
+using Content.Server.Station.Systems;
 using Content.Server.StationEvents.Components;
 using Content.Shared.Psionics.Glimmer;
 using Content.Shared.Abilities.Psionics;
+using Robust.Shared.Map;
 
 namespace Content.Server.StationEvents.Events;
 
 public sealed class GlimmerMobRule : StationEventSystem<GlimmerMobRuleComponent>
 {
     [Dependency] private readonly IRobustRandom _robustRandom = default!;
-    [Dependency] private readonly GlimmerSystem _glimmerSystem = default!;
     [Dependency] private readonly GameTicker _gameTicker = default!;
+    [Dependency] private readonly GlimmerSystem _glimmer = default!;
+    [Dependency] private readonly StationSystem _stations = default!;
 
-
-    protected override void Started(EntityUid uid, GlimmerMobRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
+    protected override void Started(EntityUid uid, GlimmerMobRuleComponent comp, GameRuleComponent gameRule, GameRuleStartedEvent args)
     {
-        base.Started(uid, component, gameRule, args);
+        base.Started(uid, comp, gameRule, args);
 
-        var station = _gameTicker.GetSpawnableStations();
-        if (station is null)
+        var stations = _gameTicker.GetSpawnableStations();
+        if (stations.Count <= 0)
             return;
 
-        var glimmerSources = new List<(GlimmerSourceComponent, TransformComponent)>();
-        foreach (var source in EntityQuery<GlimmerSourceComponent, TransformComponent>().ToList())
-        {
-            if (!station.Contains(source.Item2.Owner))
-                continue;
+        List<EntityCoordinates>
+            glimmerSources = GetCoords<GlimmerSourceComponent>(stations),
+            normalSpawns = GetCoords<VentCritterSpawnLocationComponent>(stations),
+            hiddenSpawns = GetCoords<MidRoundAntagSpawnLocationComponent>(stations);
 
-            glimmerSources.Add(source);
-        }
-
-
-        var normalSpawnLocations = new List<(VentCritterSpawnLocationComponent, TransformComponent)>();
-        foreach (var source in EntityQuery<VentCritterSpawnLocationComponent, TransformComponent>().ToList())
-        {
-            if (!station.Contains(source.Item2.Owner))
-                continue;
-
-            normalSpawnLocations.Add(source);
-        }
-
-        var hiddenSpawnLocations = new List<(MidRoundAntagSpawnLocationComponent, TransformComponent)>();
-        foreach (var source in EntityQuery<MidRoundAntagSpawnLocationComponent, TransformComponent>().ToList())
-        {
-            if (!station.Contains(source.Item2.Owner))
-                continue;
-
-            hiddenSpawnLocations.Add(source);
-        }
-
-        var baseCount = Math.Max(1, EntityQuery<PsionicComponent, NpcFactionMemberComponent>().Count() / 10);
-        int multiplier = Math.Max(1, (int) _glimmerSystem.GetGlimmerTier() - 2);
-
+        var psionics = EntityQuery<PsionicComponent, NpcFactionMemberComponent>().Count();
+        var baseCount = Math.Max(1, psionics / comp.MobsPerPsionic);
+        var multiplier = Math.Max(1, (int) _glimmer.GetGlimmerTier() - (int) comp.GlimmerTier);
         var total = baseCount * multiplier;
 
-        int i = 0;
-        while (i < total)
+        Log.Info($"Spawning {total} of {comp.MobPrototype} from {ToPrettyString(uid):rule}");
+
+        for (var i = 0; i < total; i++)
         {
-            if (glimmerSources.Count != 0 && _robustRandom.Prob(0.4f))
-            {
-                Spawn(component.MobPrototype, _robustRandom.Pick(glimmerSources).Item2.Coordinates);
-                i++;
-                continue;
-            }
-
-            if (normalSpawnLocations.Count != 0)
-            {
-                Spawn(component.MobPrototype, _robustRandom.Pick(normalSpawnLocations).Item2.Coordinates);
-                i++;
-                continue;
-            }
-
-            if (hiddenSpawnLocations.Count != 0)
-            {
-                Spawn(component.MobPrototype, _robustRandom.Pick(hiddenSpawnLocations).Item2.Coordinates);
-                i++;
-                continue;
-            }
-
-            return;
+            // if we cant get a spawn just give up
+            if (!TrySpawn(comp, glimmerSources, comp.GlimmerProb) &&
+                !TrySpawn(comp, normalSpawns, comp.NormalProb) &&
+                !TrySpawn(comp, hiddenSpawns, comp.HiddenProb))
+                return;
         }
+    }
+
+    private List<EntityCoordinates> GetCoords<T>(List<EntityUid> allowedStations) where T : IComponent
+    {
+        var coords = new List<EntityCoordinates>();
+        var query = EntityQueryEnumerator<T, TransformComponent>();
+
+        while (query.MoveNext(out var uid, out _, out var xform))
+        {
+            var station = _stations.GetOwningStation(uid, xform);
+            if (station is null || !allowedStations.Contains(station.Value))
+                continue;
+
+            coords.Add(xform.Coordinates);
+        }
+
+        return coords;
+    }
+
+    private bool TrySpawn(GlimmerMobRuleComponent comp, List<EntityCoordinates> spawns, float prob)
+    {
+        if (spawns.Count == 0 || !RobustRandom.Prob(prob))
+            return false;
+
+        var coords = RobustRandom.Pick(spawns);
+        Spawn(comp.MobPrototype, coords);
+        return true;
     }
 }
