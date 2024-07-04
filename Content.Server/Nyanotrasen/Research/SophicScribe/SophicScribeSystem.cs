8897a4b4732e16baee30be42dd9925419f4cdd0a
VMSolidus <evilexecutive@gmail.com>
2024-07-04 00:16:17 -0400

    Revert "Don't look at this please."
    This reverts commit d461f4fe176ab24b8278d17b500424c840a6348d.

--- Content.Server/Nyanotrasen/Research/SophicScribe/SophicScribeSystem.cs
+++ Content.Server/Nyanotrasen/Research/SophicScribe/SophicScribeSystem.cs
@@ -1,8 +1,5 @@
 using Content.Server.Psionics.Abilities;
 using Content.Server.Chat.Systems;
-using Content.Server.NPC.Events;
-using Content.Server.NPC.Systems;
-using Content.Server.NPC.Prototypes;
 using Content.Server.Radio.Components;
 using Content.Server.Radio.EntitySystems;
 using Content.Server.StationEvents.Events;
@@ -21,8 +18,6 @@ public sealed partial class SophicScribeSystem : EntitySystem
     [Dependency] private readonly RadioSystem _radioSystem = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
-    [Dependency] private readonly NPCConversationSystem _conversationSystem = default!;
-    protected ISawmill Sawmill = default!;
 
     public override void Update(float frameTime)
     {
@@ -56,32 +51,6 @@ public sealed partial class SophicScribeSystem : EntitySystem
 
         SubscribeLocalEvent<SophicScribeComponent, InteractHandEvent>(OnInteractHand);
         SubscribeLocalEvent<GlimmerEventEndedEvent>(OnGlimmerEventEnded);
-        SubscribeLocalEvent<SophicScribeComponent, NPCConversationGetGlimmerEvent>(OnGetGlimmer);
-    }
-
-    private void OnGetGlimmer(EntityUid uid, SophicScribeComponent component, NPCConversationGetGlimmerEvent args)
-    {
-        if (args.Text == null)
-        {
-            Sawmill.Error($"{uid} heard a glimmer reading prompt but has no text for it");
-            return;
-        }
-
-        var tier = _glimmerSystem.GetGlimmerTier() switch
-        {
-            GlimmerTier.Minimal => Loc.GetString("glimmer-reading-minimal"),
-            GlimmerTier.Low => Loc.GetString("glimmer-reading-low"),
-            GlimmerTier.Moderate => Loc.GetString("glimmer-reading-moderate"),
-            GlimmerTier.High => Loc.GetString("glimmer-reading-high"),
-            GlimmerTier.Dangerous => Loc.GetString("glimmer-reading-dangerous"),
-            _ => Loc.GetString("glimmer-reading-critical"),
-        };
-
-        var glimmerReadingText = Loc.GetString(args.Text,
-            ("glimmer", (int) Math.Round(_glimmerSystem.GlimmerOutput)), ("tier", tier));
-
-        var response = new NPCResponse(glimmerReadingText);
-        _conversationSystem.QueueResponse(uid, response);
     }
 
     private void OnInteractHand(EntityUid uid, SophicScribeComponent component, InteractHandEvent args)
@@ -113,10 +82,5 @@ public sealed partial class SophicScribeSystem : EntitySystem
             var channel = _prototypeManager.Index<RadioChannelPrototype>("Common");
             _radioSystem.SendRadioMessage(speaker, message, channel, speaker);
         }
-    }
-    public sealed partial class NPCConversationGetGlimmerEvent : NPCConversationEvent
-    {
-        [DataField]
-        public string? Text;
     }
 }
