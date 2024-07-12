teecoding/Zona-14

made the hover text less vague (sorry) (#26630)

Resources/Locale/en-US/traits/traits.ftl
@@ -34,7 +34,7 @@ trait-socialanxiety-name = Social Anxiety
 trait-socialanxiety-desc = You are anxious when you speak and stutter.
 
 trait-southern-name = Southern Drawl
-trait-southern-desc = Are you wonderin' what this does?
+trait-southern-desc = You have a different way of speakin'.
 
 trait-snoring-name = Snoring
 trait-snoring-desc = You will snore while sleeping.

Simple-Station/Einstein-Engines

Resources/Locale/en-US/traits/traits.ftl
@@ -36,7 +36,7 @@ trait-name-NormalVisionHarpy = Trichromat Modification
 trait-description-NormalVisionHarpy = Your eyes have been modified by means of advanced medicine to see in the standard colors of Red, Green, and Blue.
 
 trait-name-Southern = Southern Drawl
-trait-description-Southern = Are you wonderin' what this does?
+trait-description-Southern = You have a different way of speakin'.
 
 trait-name-NormalVisionVulpkanin = Trichromat Modification
 trait-description-NormalVisionVulpkanin = Your eyes have been modified by means of advanced medicine to see in the standard colors of Red, Green, and Blue.