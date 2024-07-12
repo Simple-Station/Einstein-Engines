teecoding/Zona-14

Southern accent (#26543)
* created the AccentComponent and the AccentSystem

* word replacement schtuhff

* made it a trait fr ongg!!1

* Update Content.Server/Speech/EntitySystems/SouthernAccentSystem.cs

---------

Co-authored-by: metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>

Resources/Locale/en-US/traits/traits.ftl
@@ -33,5 +33,8 @@ trait-frontal-lisp-desc = You thpeak with a lithp
 trait-socialanxiety-name = Social Anxiety
 trait-socialanxiety-desc = You are anxious when you speak and stutter.
 
+trait-southern-name = Southern Drawl
+trait-southern-desc = Are you wonderin' what this does?
+
 trait-snoring-name = Snoring
 trait-snoring-desc = You will snore while sleeping.

Simple-Station/Einstein-Engines

Resources/Locale/en-US/traits/traits.ftl
@@ -35,6 +35,9 @@ trait-description-Snoring = You will snore while sleeping.
 trait-name-NormalVisionHarpy = Trichromat Modification
 trait-description-NormalVisionHarpy = Your eyes have been modified by means of advanced medicine to see in the standard colors of Red, Green, and Blue.
 
+trait-name-Southern = Southern Drawl
+trait-description-Southern = Are you wonderin' what this does?
+
 trait-name-NormalVisionVulpkanin = Trichromat Modification
 trait-description-NormalVisionVulpkanin = Your eyes have been modified by means of advanced medicine to see in the standard colors of Red, Green, and Blue.
 