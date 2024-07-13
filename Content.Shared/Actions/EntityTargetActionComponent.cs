d21cf9db8961094743e9a283dbb333155680b1ae
ShadowCommander <10494922+shadowcommander@users.noreply.github.com>
2024-07-13 07:04:53 +0300

    Add doc comments to target action components (#29982)
    (cherry picked from commit b94b52396e52e7627d80495f019a89a51751c6a8)

--- Content.Shared/Actions/EntityTargetActionComponent.cs
+++ Content.Shared/Actions/EntityTargetActionComponent.cs
@@ -4,6 +4,9 @@ using Robust.Shared.Serialization;
 
 namespace Content.Shared.Actions;
 
+/// <summary>
+/// Used on action entities to define an action that triggers when targeting an entity.
+/// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class EntityTargetActionComponent : BaseTargetActionComponent
 {
@@ -16,9 +19,17 @@ public sealed partial class EntityTargetActionComponent : BaseTargetActionCompon
     [NonSerialized]
     public EntityTargetActionEvent? Event;
 
+    /// <summary>
+    /// Determines which entities are valid targets for this action.
+    /// </summary>
+    /// <remarks>No whitelist check when null.</remarks>
     [DataField] public EntityWhitelist? Whitelist;
+
     [DataField] public EntityWhitelist? Blacklist;
 
+    /// <summary>
+    /// Whether this action considers the user as a valid target entity when using this action.
+    /// </summary>
     [DataField] public bool CanTargetSelf = true;
 }
 
