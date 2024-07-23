daf6bf3178cc4317e5a075cad618b821a62e0634
fox <daytimer253@gmail.com>
2024-07-23 12:10:16 +0300

    Undo the suggestion

--- Content.Shared/Actions/Events/FabricateActionEvent.cs
+++ Content.Shared/Actions/Events/FabricateActionEvent.cs
@@ -2,7 +2,7 @@ using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Actions.Events;
 
-public sealed class FabricateActionEvent : InstantActionEvent
+public sealed partial class FabricateActionEvent : InstantActionEvent
 {
     [DataField(required: true)]
     public ProtoId<EntityPrototype> Fabrication;
