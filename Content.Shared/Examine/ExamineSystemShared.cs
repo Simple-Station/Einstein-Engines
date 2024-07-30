c7958d60f680ebee015cc321c30e3ee0c564009e
Mervill <mervills.email@gmail.com>
2024-07-29 17:57:46 -0700

    Objects that only have a basic description won't have extra line in examine text (#30472)
    objects that only have a basic description won't have extra line in examine text
    
    (cherry picked from commit 83a56cc74afb57970cea480efc43ac1a9621a5a8)

--- Content.Shared/Examine/ExamineSystemShared.cs
+++ Content.Shared/Examine/ExamineSystemShared.cs
@@ -371,7 +371,7 @@ namespace Content.Shared.Examine
             var totalMessage = new FormattedMessage(Message);
             parts.Sort(Comparison);
 
-            if (_hasDescription)
+            if (_hasDescription && parts.Count > 0)
             {
                 totalMessage.PushNewline();
             }
