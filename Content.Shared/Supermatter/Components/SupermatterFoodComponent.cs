af52d7151481b95f3b6e5881a7c9bd30cf96345b
VMSolidus <evilexecutive@gmail.com>
2024-07-30 19:51:36 -0400

    Update SupermatterFoodComponent.cs

--- Content.Shared/Supermatter/Components/SupermatterFoodComponent.cs
+++ Content.Shared/Supermatter/Components/SupermatterFoodComponent.cs
@@ -3,7 +3,6 @@ namespace Content.Shared.Supermatter.Components;
 [RegisterComponent]
 public sealed partial class SupermatterFoodComponent : Component
 {
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("energy")]
+    [DataField]
     public int Energy { get; set; } = 1;
 }
