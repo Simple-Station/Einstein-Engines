a40ee1cfbcc587b058346a4b8629a1a9ebb83ce5
VMSolidus <evilexecutive@gmail.com>
2024-07-30 19:19:22 -0400

    Update GeigerSystem.cs

--- Content.Server/Radiation/Systems/GeigerSystem.cs
+++ Content.Server/Radiation/Systems/GeigerSystem.cs
@@ -6,7 +6,6 @@ using Content.Shared.Inventory.Events;
 using Content.Shared.Radiation.Components;
 using Content.Shared.Radiation.Systems;
 using Robust.Server.Audio;
-using Robust.Server.GameObjects;
 using Robust.Server.Player;
 using Robust.Shared.Player;
 
