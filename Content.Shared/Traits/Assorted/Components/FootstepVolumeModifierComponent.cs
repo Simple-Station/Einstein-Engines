adedcba5b49d924ac07faf88ba98e60aff5c2656
Angelo Fallaria <ba.fallaria@gmail.com>
2024-08-03 13:39:42 +0800

    feat(traits): start work on Light Step
    TODO: make the client controlling the Light Steps entity also hear
    quieter footsteps instead of regular volume footsteps

---
+++ Content.Shared/Traits/Assorted/Components/FootstepVolumeModifierComponent.cs
@@ -0,0 +1,22 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Traits.Assorted.Components;
+
+/// <summary>
+///     This is used for any trait that modifies footstep volumes.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class FootstepVolumeModifierComponent : Component
+{
+    /// <summary>
+    ///     What to add to the volume of sprinting.
+    /// </summary>
+    [DataField]
+    public float SprintingModifier { get; set; } = 0f;
+
+    /// <summary>
+    ///     What to add to the volume of walking.
+    /// </summary>
+    [DataField]
+    public float WalkingModifier { get; set; } = 0f;
+}
---
+++ Content.Shared/Traits/Assorted/Systems/FootstepVolumeModifierSystem.cs
@@ -0,0 +1,5 @@
+namespace Content.Shared.Traits.Assorted.Systems;
+
+public sealed class FootstepVolumeModifierSystem : EntitySystem
+{
+}
