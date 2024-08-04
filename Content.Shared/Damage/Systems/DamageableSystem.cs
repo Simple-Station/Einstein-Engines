eba893f0fd556798c0bcc91e527525699c044573
sanek31 <149935707+sanek31@users.noreply.github.com>
2024-08-04 15:32:10 +0300

    Update DamageableSystem.cs  - float? to float
    Signed-off-by: sanek31 <149935707+sanek31@users.noreply.github.com>

--- Content.Shared/Damage/Systems/DamageableSystem.cs
+++ Content.Shared/Damage/Systems/DamageableSystem.cs
@@ -125,7 +125,7 @@ namespace Content.Shared.Damage
         ///     null if the user had no applicable components that can take damage.
         /// </returns>
         public DamageSpecifier? TryChangeDamage(EntityUid? uid, DamageSpecifier damage, bool ignoreResistances = false,
-            bool interruptsDoAfters = true, DamageableComponent? damageable = null, EntityUid? origin = null, float? bypassResistance = null)
+            bool interruptsDoAfters = true, DamageableComponent? damageable = null, EntityUid? origin = null, float bypassResistance = 1)
         {
             if (!uid.HasValue || !_damageableQuery.Resolve(uid.Value, ref damageable, false))
             {
