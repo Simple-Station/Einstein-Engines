d8464f5dcbee110c28c8d38fdc16143e1a728ae4
Angelo Fallaria <ba.fallaria@gmail.com>
2024-08-04 11:01:19 +0800

    tweak(traits) set FootstepVolumeModifierComponent setters to private

--- Content.Shared/Traits/Assorted/Components/FootstepVolumeModifierComponent.cs
+++ Content.Shared/Traits/Assorted/Components/FootstepVolumeModifierComponent.cs
@@ -12,11 +12,11 @@ public sealed partial class FootstepVolumeModifierComponent : Component
     ///     What to add to the volume of sprinting, in terms of decibels.
     /// </summary>
     [DataField, AutoNetworkedField]
-    public float SprintVolumeModifier { get; set; } = 0f;
+    public float SprintVolumeModifier { get; private set; } = 0f;
 
     /// <summary>
     ///     What to add to the volume of walking, in terms of decibels.
     /// </summary>
     [DataField, AutoNetworkedField]
-    public float WalkVolumeModifier { get; set; } = 0f;
+    public float WalkVolumeModifier { get; private set; } = 0f;
 }
