ca22ddeb43890c5a0189d6168d5b7edfb7b65d3e
whateverusername0 <whateveremail>
2024-08-06 14:40:51 +1000

    god save my soul

--- Content.Server/Nyanotrasen/Psionics/PsionicsSystem.cs
+++ Content.Server/Nyanotrasen/Psionics/PsionicsSystem.cs
@@ -51,7 +51,7 @@ namespace Content.Server.Psionics
             base.Initialize();
             SubscribeLocalEvent<PotentialPsionicComponent, MapInitEvent>(OnStartup);
             SubscribeLocalEvent<AntiPsionicWeaponComponent, MeleeHitEvent>(OnMeleeHit);
-            SubscribeLocalEvent<AntiPsionicWeaponComponent, StaminaMeleeHitEvent>(OnStamHit);
+            SubscribeLocalEvent<AntiPsionicWeaponComponent, TakeStaminaDamageEvent>(OnStamHit);
 
             SubscribeLocalEvent<PsionicComponent, ComponentInit>(OnInit);
             SubscribeLocalEvent<PsionicComponent, ComponentRemove>(OnRemove);
@@ -110,7 +110,7 @@ namespace Content.Server.Psionics
             _npcFactonSystem.RemoveFaction(uid, "PsionicInterloper");
         }
 
-        private void OnStamHit(EntityUid uid, AntiPsionicWeaponComponent component, StaminaMeleeHitEvent args)
+        private void OnStamHit(EntityUid uid, AntiPsionicWeaponComponent component, TakeStaminaDamageEvent args)
         {
             var bonus = false;
             foreach (var stam in args.HitList)
