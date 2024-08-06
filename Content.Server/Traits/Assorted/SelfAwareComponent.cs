2267437c7db25765b52711a5b2d448043aeaa796
Angelo Fallaria <ba.fallaria@gmail.com>
2024-08-06 13:23:45 +0800

    style(traits): add spaces in summary comments
    Co-authored-by: VMSolidus <evilexecutive@gmail.com>
    Signed-off-by: Angelo Fallaria <ba.fallaria@gmail.com>

--- Content.Server/Traits/Assorted/SelfAwareComponent.cs
+++ Content.Server/Traits/Assorted/SelfAwareComponent.cs
@@ -5,25 +5,25 @@ using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototy
 namespace Content.Server.Traits.Assorted;
 
 /// <summary>
-/// This is used for the Self-Aware trait to enhance the information received from HealthExaminableSystem.
+///     This is used for the Self-Aware trait to enhance the information received from HealthExaminableSystem.
 /// </summary>
 [RegisterComponent]
 public sealed partial class SelfAwareComponent : Component
 {
     // <summary>
-    // Damage types that an entity is able to precisely analyze like a health analyzer when they examine themselves.
+    //     Damage types that an entity is able to precisely analyze like a health analyzer when they examine themselves.
     // </summary>
     [DataField("analyzableTypes", required: true, customTypeSerializer:typeof(PrototypeIdHashSetSerializer<DamageTypePrototype>))]
     public HashSet<string> AnalyzableTypes = default!;
 
     // <summary>
-    // Damage groups that an entity is able to detect the presence of when they examine themselves.
+    //     Damage groups that an entity is able to detect the presence of when they examine themselves.
     // </summary>
     [DataField("detectableGroups", required: true, customTypeSerializer:typeof(PrototypeIdHashSetSerializer<DamageGroupPrototype>))]
     public HashSet<string> DetectableGroups = default!;
 
     // <summary>
-    // The thresholds for DetectableGroups.
+    //     The thresholds for DetectableGroups.
     // </summary>
     public List<FixedPoint2> Thresholds = new()
         { FixedPoint2.New(10), FixedPoint2.New(25), FixedPoint2.New(50), FixedPoint2.New(75) };
