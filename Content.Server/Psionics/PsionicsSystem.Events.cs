f858f95a5cd1729f96f79b34fbb84e3ff28a939e
VMSolidus <evilexecutive@gmail.com>
2024-08-18 17:00:58 -0400

    Update PsionicsSystem.Events.cs

--- Content.Server/Psionics/PsionicsSystem.Events.cs
+++ Content.Server/Psionics/PsionicsSystem.Events.cs
@@ -1,17 +1,19 @@
 
-namespace Content.Server.Psionics;
-
-/// <summary>
-///     Raised on an entity about to roll for a Psionic Power, after their baseline chances of success are calculated.
-/// </summary>
-[ByRefEvent]
-public struct OnRollPsionicsEvent
+namespace Content.Server.Psionics
 {
-    public readonly EntityUid Roller;
-    public float BaselineChance;
-    public OnRollPsionicsEvent(EntityUid roller, float baselineChance)
+    /// <summary>
+    ///     Raised on an entity about to roll for a Psionic Power, after their baseline chances of success are calculated.
+    /// </summary>
+    [ByRefEvent]
+    public struct OnRollPsionicsEvent
     {
-        Roller = roller;
-        BaselineChance = baselineChance;
+        public readonly EntityUid Roller;
+        public float BaselineChance;
+        public OnRollPsionicsEvent(EntityUid roller, float baselineChance)
+        {
+            Roller = roller;
+            BaselineChance = baselineChance;
+        }
     }
 }
+
