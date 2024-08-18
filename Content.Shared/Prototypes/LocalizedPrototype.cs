bf860de5a1140a2208e5c7d30e8fb0d38e8a41e8
DEATHB4DEFEAT <zachcaffee@outlook.com>
2024-08-18 01:08:01 -0700

    LocalizedPrototype

---
+++ Content.Shared/Prototypes/LocalizedPrototype.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Prototypes;
+
+public abstract class LocalizedPrototype : IPrototype
+{
+    [IdDataField]
+    public string ID { get; } = default!;
+
+    public const string LocFormat = "{type}-{ID}-{field}";
+
+    public string Name => ToLocalizationString("name");
+    public string LocalizedName => Loc.GetString(Name);
+
+    public string ToLocalizationString(string field)
+    {
+        // Get the ID of the proto Type
+        var type =
+            ((PrototypeAttribute?) Attribute.GetCustomAttribute(GetType(), typeof(PrototypeAttribute)))?.Type
+            ?? GetType().Name.Remove(GetType().Name.Length - 9);
+        // Lowercase the first letter
+        type = char.ToLowerInvariant(type[0]) + type[1..];
+        // Replace every uppercase letter with a dash and the lowercase letter
+        type = type.Aggregate("", (current, c) => current + (char.IsUpper(c) ? "-" + char.ToLowerInvariant(c) : c.ToString()));
+
+        // Replace the placeholders with the actual values
+        var t = LocFormat
+            .Replace("{type}", type)
+            .Replace("{ID}", ID)
+            .Replace("{field}", field);
+
+        Logger.Fatal(t);
+        Logger.Fatal(Loc.GetString(t));
+        return t;
+    }
+}
