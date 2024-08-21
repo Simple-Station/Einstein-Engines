ad1d76eb87eb363c3cf88beafe2ec9f5146bfb15
Timemaster99 <57200767+Timemaster99@users.noreply.github.com>
2024-08-21 05:14:30 -0400

    DeltaV/DS14 IPC Port (#744)
    <!--
    This is a semi-strict format, you can add/remove sections as needed but
    the order/format should be kept the same
    Remove these comments before submitting
    -->
    
    # Description
    
    <!--
    Explain this PR in as much detail as applicable
    
    Some example prompts to consider:
    How might this affect the game? The codebase?
    What might be some alternatives to this?
    How/Who does this benefit/hurt [the game/codebase]?
    -->
    
    Ported over IPC from DS14, with the fixes from DeltaV.
    
    ---
    
    # TODO
    
    <!--
    A list of everything you have to do before this PR is "complete"
    You probably won't have to complete everything before merging but it's
    good to leave future references
    -->
    
    - [X] Port
    - [ ] Check for errors (Local tests wouldn't run on this one)
    
    ---
    
    <!--
    This is default collapsed, readers click to expand it and see all your
    media
    The PR media section can get very large at times, so this is a good way
    to keep it clean
    The title is written using HTML tags
    The title must be within the <summary> tags or you won't see it
    -->
    
    <details><summary><h1>Media</h1></summary>
    <p>
    
    
    
    </p>
    </details>
    
    ---
    
    # Changelog
    
    <!--
    You can add an author after the `:cl:` to change the name that appears
    in the changelog (ex: `:cl: Death`)
    Leaving it blank will default to your GitHub display name
    This includes all available types for the changelog
    -->
    
    :cl:
    - add: Added IPC as a playable species.
    
    ---------
    
    Signed-off-by: Timemaster99 <57200767+Timemaster99@users.noreply.github.com>
    Signed-off-by: VMSolidus <evilexecutive@gmail.com>
    Co-authored-by: Daniela <43686351+Day-OS@users.noreply.github.com>
    Co-authored-by: Danger Revolution! <142105406+DangerRevolution@users.noreply.github.com>
    Co-authored-by: VMSolidus <evilexecutive@gmail.com>

--- Content.Server/Station/Systems/StationSpawningSystem.cs
+++ Content.Server/Station/Systems/StationSpawningSystem.cs
@@ -5,6 +5,7 @@ using Content.Server.IdentityManagement;
 using Content.Server.Mind.Commands;
 using Content.Server.PDA;
 using Content.Server.Shuttles.Systems;
+using Content.Server.Silicon.IPC;
 using Content.Server.Spawners.EntitySystems;
 using Content.Server.Spawners.Components;
 using Content.Server.Station.Components;
@@ -84,7 +85,6 @@ public sealed class StationSpawningSystem : SharedStationSpawningSystem
         if (station != null && !Resolve(station.Value, ref stationSpawning))
             throw new ArgumentException("Tried to use a non-station entity as a station!", nameof(station));
 
-        // Delta-V: Set desired spawn point type.
         var ev = new PlayerSpawningEvent(job, profile, station, spawnPointType);
 
         if (station != null && profile != null)
@@ -176,6 +176,7 @@ public sealed class StationSpawningSystem : SharedStationSpawningSystem
             EquipStartingGear(entity.Value, startingGear, profile);
             if (profile != null)
                 EquipIdCard(entity.Value, profile.Name, prototype, station);
+                InternalEncryptionKeySpawner.TryInsertEncryptionKey(entity.Value, startingGear, EntityManager, profile); // Parkstation - IPC
         }
 
         if (profile != null)
@@ -274,7 +275,7 @@ public sealed class PlayerSpawningEvent : EntityEventArgs
     /// </summary>
     public readonly EntityUid? Station;
     /// <summary>
-    /// Delta-V: Desired SpawnPointType, if any.
+    /// Desired SpawnPointType, if any.
     /// </summary>
     public readonly SpawnPointType DesiredSpawnPointType;
 
