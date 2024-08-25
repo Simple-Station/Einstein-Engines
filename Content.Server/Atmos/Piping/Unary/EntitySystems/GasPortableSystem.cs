9fc94e11bccba09f5d892af74fd08ee3eda9369f
Mervill <mervills.email@gmail.com>
2024-08-25 04:55:02 +0300

    Replace obsolete code in GasPortableSystem (#31407)
    (cherry picked from commit 059c84c2cf2cea141af67c9e347dcaadecfbc2b5)

--- Content.Server/Atmos/Piping/Unary/EntitySystems/GasPortableSystem.cs
+++ Content.Server/Atmos/Piping/Unary/EntitySystems/GasPortableSystem.cs
@@ -15,9 +15,9 @@ namespace Content.Server.Atmos.Piping.Unary.EntitySystems
     [UsedImplicitly]
     public sealed class GasPortableSystem : EntitySystem
     {
+        [Dependency] private readonly SharedMapSystem _mapSystem = default!;
         [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
         [Dependency] private readonly NodeContainerSystem _nodeContainer = default!;
-        [Dependency] private readonly SharedMapSystem _sharedMapSystem = default!;
 
         public override void Initialize()
         {
@@ -55,13 +55,10 @@ namespace Content.Server.Atmos.Piping.Unary.EntitySystems
         {
             port = null;
 
-            if (gridId == null)
-                return false;
-
             if (!TryComp<MapGridComponent>(gridId, out var grid))
                 return false;
 
-            foreach (var entityUid in _sharedMapSystem.GetLocal((EntityUid) gridId, grid, coordinates))
+            foreach (var entityUid in _mapSystem.GetLocal(gridId.Value, grid, coordinates))
             {
                 if (EntityManager.TryGetComponent(entityUid, out port))
                 {
