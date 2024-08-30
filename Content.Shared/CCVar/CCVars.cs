577df878cfaaea09ff2e61c1ba1b1222b70e275f
VMSolidus <evilexecutive@gmail.com>
2024-08-29 22:49:24 -0400

    Update CCVars.cs

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -2474,7 +2474,12 @@ namespace Content.Shared.CCVar
 
         #region Material Reclaimer
 
+        /// <summary>
+        ///     Whether or not a Material Reclaimer is allowed to eat people when emagged.
+        /// </summary>
         public static readonly CVarDef<bool> ReclaimerAllowGibbing =
             CVarDef.Create("reclaimer.allow_gibbing", true, CVar.SERVER);
+
+        #endregion
     }
 }
