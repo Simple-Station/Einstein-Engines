54960b75c7aeec5c2765d0f7c983fdfa8c0d0314
VMSolidus <evilexecutive@gmail.com>
2024-09-01 18:34:02 -0400

    Small fixes.

--- Content.Client/Standing/LayingDownSystem.cs
+++ Content.Client/Standing/LayingDownSystem.cs
@@ -1,4 +1,4 @@
-using Content.Shared.Buckle;
+using Content.Shared.ActionBlocker;
 using Content.Shared.Rotation;
 using Content.Shared.Standing;
 using Robust.Client.GameObjects;
@@ -11,9 +11,8 @@ public sealed class LayingDownSystem : SharedLayingDownSystem
 {
     [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly IEyeManager _eyeManager = default!;
-    [Dependency] private readonly StandingStateSystem _standing = default!;
     [Dependency] private readonly AnimationPlayerSystem _animation = default!;
-    [Dependency] private readonly SharedBuckleSystem _buckle = default!;
+    [Dependency] private readonly ActionBlockerSystem _actionBlocker = default!;
 
     public override void Initialize()
     {
@@ -27,8 +26,7 @@ public sealed class LayingDownSystem : SharedLayingDownSystem
     private void OnMovementInput(EntityUid uid, LayingDownComponent component, MoveEvent args)
     {
         if (!_timing.IsFirstTimePredicted
-            || !_standing.IsDown(uid)
-            || _buckle.IsBuckled(uid)
+            || !_actionBlocker.CanMove(uid)
             || _animation.HasRunningAnimation(uid, "rotate")
             || !TryComp<TransformComponent>(uid, out var transform)
             || !TryComp<SpriteComponent>(uid, out var sprite)
