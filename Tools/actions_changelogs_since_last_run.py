0b82f23a0281a7ae25f792f034c567d1c8bee990
DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>
2024-09-01 16:46:09 -0700

    Update actions_changelogs_since_last_run.py
    Signed-off-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>

--- Tools/actions_changelogs_since_last_run.py
+++ Tools/actions_changelogs_since_last_run.py
@@ -15,7 +15,7 @@ from typing import Any, Iterable
 GITHUB_API_URL    = os.environ.get("GITHUB_API_URL", "https://api.github.com")
 GITHUB_REPOSITORY = os.environ["GITHUB_REPOSITORY"]
 GITHUB_RUN        = os.environ["GITHUB_RUN_ID"]
-BOT_TOKEN         = os.environ["BOT_TOKEN"]
+GITHUB_TOKEN      = os.environ["GITHUB_TOKEN"]
 CHANGELOG_DIR     = os.environ["CHANGELOG_DIR"]
 CHANGELOG_WEBHOOK = os.environ["CHANGELOG_WEBHOOK"]
 
@@ -26,7 +26,7 @@ CHANGELOG_FILE = "{CHANGELOG_DIR}"
 
 TYPES_TO_EMOJI = {
     "Fix":    "🐛",
-    "Add":    "✨",
+    "Add":    "🆕",
     "Remove": "❌",
     "Tweak":  "⚒️"
 }
@@ -38,7 +38,7 @@ def main():
         return
 
     session = requests.Session()
-    session.headers["Authorization"]        = f"Bearer {BOT_TOKEN}"
+    session.headers["Authorization"]        = f"Bearer {GITHUB_TOKEN}"
     session.headers["Accept"]               = "Accept: application/vnd.github+json"
     session.headers["X-GitHub-Api-Version"] = "2022-11-28"
 
