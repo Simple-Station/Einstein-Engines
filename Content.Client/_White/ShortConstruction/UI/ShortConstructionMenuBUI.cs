01c397f0688c007078ec55afdcc943157cb0d02c
Remuchi <72476615+Remuchi@users.noreply.github.com>
2024-09-03 12:39:50 +0700

    [Feature] Short Construction System / Система Крафтов В Руках (#17)
    * feat: inhand crafting base
    
    * fix: be gone
    
    * tweak: better naming, menu stays open on item crafting
    
    * feat: recipes

---
+++ Content.Client/_White/ShortConstruction/UI/ShortConstructionMenuBUI.cs
@@ -0,0 +1,33 @@
+﻿using JetBrains.Annotations;
+using Robust.Client.Graphics;
+using Robust.Client.Input;
+
+// ReSharper disable InconsistentNaming
+
+namespace Content.Client._White.ShortConstruction.UI;
+
+[UsedImplicitly]
+public sealed class ShortConstructionMenuBUI(EntityUid owner, Enum uiKey) : BoundUserInterface(owner, uiKey)
+{
+    [Dependency] private readonly IClyde _displayManager = default!;
+    [Dependency] private readonly IInputManager _inputManager = default!;
+    private ShortConstructionMenu? _menu;
+
+    protected override void Open()
+    {
+        _menu = new ShortConstructionMenu(Owner);
+        _menu.OnClose += Close;
+
+        var vpSize = _displayManager.ScreenSize;
+        _menu.OpenCenteredAt(_inputManager.MouseScreenPosition.Position / vpSize);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (!disposing)
+            return;
+
+        _menu?.Dispose();
+    }
+}
---
+++ Content.Shared/_White/ShortConstruction/ShortConstructionComponent.cs
@@ -0,0 +1,19 @@
+﻿using Content.Shared.Construction.Prototypes;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._White.ShortConstruction;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ShortConstructionComponent : Component
+{
+    [DataField(required: true)]
+    public List<ProtoId<ConstructionPrototype>> Prototypes = new();
+}
+
+[NetSerializable, Serializable]
+public enum ShortConstructionUiKey : byte
+{
+    Key,
+}
