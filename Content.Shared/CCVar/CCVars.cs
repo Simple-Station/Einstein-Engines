6b68e80e1b1409e1996f779b2dd024d4d6b42eee
Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>
2024-09-05 06:08:16 +0300

    Update Content.Shared/CCVar/CCVars.cs
    Co-authored-by: VMSolidus <evilexecutive@gmail.com>
    Signed-off-by: Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -2297,7 +2297,9 @@ namespace Content.Shared.CCVar
         /// <summary>
         ///     Whether height & width sliders adjust a player's bloodstream volume.
         /// </summary>
-        /// <remarks>This can be configured more precisely by modifying BloodstreamAffectedByMassComponent.</remarks>
+        /// <remarks>
+        ///     This can be configured more precisely by modifying BloodstreamAffectedByMassComponent.
+        /// </remarks>
         public static readonly CVarDef<bool> HeightAdjustModifiesBloodstream =
             CVarDef.Create("heightadjust.modifies_bloodstream", true, CVar.SERVERONLY);
 
