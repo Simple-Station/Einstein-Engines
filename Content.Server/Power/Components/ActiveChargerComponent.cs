26050033e96a83dc1f9d69d3b9bcdc781e3bb529
Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>
2024-09-08 17:38:32 +0300

    Update Content.Server/Power/Components/ActiveChargerComponent.cs
    Co-authored-by: VMSolidus <evilexecutive@gmail.com>
    Signed-off-by: Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>

--- Content.Server/Power/Components/ActiveChargerComponent.cs
+++ Content.Server/Power/Components/ActiveChargerComponent.cs
@@ -1,10 +1,7 @@
 using Content.Shared.Containers.ItemSlots;
 using Content.Shared.Power;
 
-namespace Content.Server.Power.Components
-{
-    [RegisterComponent]
-    public sealed partial class ActiveChargerComponent : Component
-    {
-    }
-}
+namespace Content.Server.Power.Components;
+
+[RegisterComponent]
+public sealed partial class ActiveChargerComponent : Component { }
