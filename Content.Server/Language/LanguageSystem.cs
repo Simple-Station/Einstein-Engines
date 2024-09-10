1f51cdf9570804293f4078cd44cf453189cecc09
VMSolidus <evilexecutive@gmail.com>
2024-09-09 22:48:43 -0400

    Update LanguageSystem.cs

--- Content.Server/Language/LanguageSystem.cs
+++ Content.Server/Language/LanguageSystem.cs
@@ -21,11 +21,15 @@ public sealed partial class LanguageSystem : SharedLanguageSystem
 
     private void OnUniversalLanguageInit(EntityUid uid, UniversalLanguageSpeakerComponent component, MapInitEvent args)
     {
+        EnsureComp<LanguageKnowledgeComponent>(uid);
         AddLanguage(uid, UniversalPrototype);
     }
 
     private void OnUniversalLanguageShutdown(EntityUid uid, UniversalLanguageSpeakerComponent component, ComponentShutdown args)
     {
+        if (!HasComp<LanguageKnowledgeComponent>(uid))
+            return;
+
         RemoveLanguage(uid, UniversalPrototype);
     }
 
