RebelledCombine/Zona-14

Pathological Liar (#27618)
* content

* upgrade

* n't

* ye ya

* Update speech-liar.ftl

* Mith replacement ideas

* fix

* more!

* Revert "more!"

This reverts commit 6d10bdf694985c525a2b451ed39380f975059b44.

* Update Content.Server/Speech/Components/ReplacementAccentComponent.cs

---------

Co-authored-by: Kara <lunarautomaton6@gmail.com>

Resources/Locale/en-US/traits/traits.ftl
@@ -38,3 +38,6 @@ trait-southern-desc = You have a different way of speakin'.
 
 trait-snoring-name = Snoring
 trait-snoring-desc = You will snore while sleeping.
+
+trait-liar-name = Pathological liar
+trait-liar-desc = You can hardly bring yourself to tell the truth. Sometimes you lie anyway.

Simple-Station/Einstein-Engines

(cherry picked from commit 5ab1cc0c846775e438517bbff9d7713eeb5ef85d)

Resources/Locale/en-US/traits/traits.ftl
@@ -231,3 +231,6 @@ trait-description-AnomalousPositronics =
 trait-name-AddictionNicotine = Nicotine Addiction
 trait-description-AddictionNicotine =
     You have an addiction to Nicotine, and will require frequent smoke breaks to keep your mood in check.
+
+trait-name-Liar = Pathological liar
+trait-description-Liar = You can hardly bring yourself to tell the truth. Sometimes you lie anyway.