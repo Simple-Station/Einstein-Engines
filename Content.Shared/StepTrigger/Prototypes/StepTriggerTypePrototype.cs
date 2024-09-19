dee5a409aecd50ded7610f7c6f8d727f4b76e97e
VMSolidus <evilexecutive@gmail.com>
2024-09-18 23:29:41 -0400

    [Port] StepTriggerGroup From WhiteDream (#929)
    # Description
    
    This is a port of
    https://github.com/WWhiteDreamProject/wwdpublic/pull/53 from White
    Dream. This PR improves the StepTriggerImmune component by making it
    operate on a more granular Blacklist system, such that StepTriggerImmune
    entities can further clarify via prototypes which kinds of floor traps
    they are immune to, such as landmines/mousetraps, and not have blanket
    immunity to everything. Because it turns out things like Lava and Soap
    also were caught by the immunity, when really we just wanted Harpies &
    Felinids to not trigger landmines.
    
    
    
    <details><summary><h1>Media</h1></summary>
    <p>
    
    > # Описание
    > Необходимо настроить модификатор урона, чтобы IPC не получали урон от
    осколков стекла.
    > 
    > Иммунитет StepTriggerImmuneComponent доработан. Теперь имеются
    несколько типов (types): Lava - тип тайла, наступив на который
    появляется урон. Это собственно лава и LiquidPlasma Landmine - мины.
    Chasm - дырка в карте, куда можно провалиться Mousetrap - Мышеловка
    SlipTile - Все, что должно подскальзывать игроков, имеющее размер тайла
    SlipEntity - Все, что должно подскальзывать игроков, имеющее развер
    энтити. Разделено для баланса. Самые ловки могут игнорировать мелкие
    предметы (энтити), т.е. уворачиваться от них. Но большие по площади вещи
    (тайлы по типу разлитой воды, бананиума) просчитываются отдельно.
    > 
    > # Изменения
    > * [x]  Улучшить StepTriggerSystem (Immune)
    > * [x] Добавлены типы триггера. - Lava Landmine Shard Chasm Mousetrap
    SlipTile SlipEntity
    > * [x]  Исправить осколки у IPC
    > * [x] Исправить отсутствие урона от лавы и падение в дыры у фелинидов
    и гарпий.
    > 
    > 🆑 Hell_Cat
    > 
    > * Feature: StepTriggerSystem is improved | Улучшена StepTriggerSystem
    > * fix: IPC: Immunity for shards and SpiderWeb | Иммунитет осколкам.
    > * fix: Felinid | Фелиниды : Immunity for Shard Landmine Mousetrap
    SlipEntities | Иммунитет для осколков, жидкости, мин, мышеловок, мыла и
    бананов.
    > * fix: Harpy | Гарпия : Immunity for Shards Landmine Mousetrap |
    Иммунитет для осколков, жидкости, мин и мышеловок.
    > * fix: Mice | Мыши : Don't blow up on landmines | Мыши не подрываются
    на минах.
    
    </p>
    </details>
    
    # Changelog
    
    :cl: Hell_Cat
    Feature: StepTriggerSystem has been improved with new StepTriggerGroups.
    Additionally, the StepTriggerImmune component now allows declaring for
    specific StepTriggerGroups for a given entity to be immune to. Some
    examples may be, Felinids, Mice, and Harpies being unable to set off
    Landmines.
    
    ---------
    
    Signed-off-by: VMSolidus <evilexecutive@gmail.com>
    Co-authored-by: Ivan <126400932+HellCatten@users.noreply.github.com>
    Co-authored-by: FoxxoTrystan <45297731+FoxxoTrystan@users.noreply.github.com>

---
+++ Content.Shared/StepTrigger/Prototypes/StepTriggerTypePrototype.cs
@@ -0,0 +1,15 @@
+﻿using Robust.Shared.Prototypes;
+
+namespace Content.Shared.StepTrigger.Prototypes;
+
+/// <summary>
+///     Prototype representing a StepTriggerType in YAML.
+///     Meant to only have an ID property, as that is the only thing that
+///     gets saved in StepTriggerGroup.
+/// </summary>
+[Prototype]
+public sealed partial class StepTriggerTypePrototype : IPrototype
+{
+    [ViewVariables, IdDataField]
+    public string ID { get; private set; } = default!;
+}
