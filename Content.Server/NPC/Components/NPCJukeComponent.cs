a7a07be18141fd1502d5d6447a20a20cf783d77c
VMSolidus <evilexecutive@gmail.com>
2024-09-19 20:22:02 -0400

    Less Aggressive Juking

--- Content.Server/NPC/Components/NPCJukeComponent.cs
+++ Content.Server/NPC/Components/NPCJukeComponent.cs
@@ -1,4 +1,3 @@
-using Content.Server.NPC.HTN.PrimitiveTasks.Operators.Combat;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
 namespace Content.Server.NPC.Components;
@@ -6,17 +5,20 @@ namespace Content.Server.NPC.Components;
 [RegisterComponent, AutoGenerateComponentPause]
 public sealed partial class NPCJukeComponent : Component
 {
-    [DataField("jukeType")]
+    [DataField]
     public JukeType JukeType = JukeType.Away;
 
-    [DataField("jukeDuration")]
+    [DataField]
     public float JukeDuration = 0.5f;
 
-    [DataField("nextJuke", customTypeSerializer:typeof(TimeOffsetSerializer))]
+    [DataField]
+    public float JukeCooldown = 3f;
+
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
     [AutoPausedField]
     public TimeSpan NextJuke;
 
-    [DataField("targetTile")]
+    [DataField]
     public Vector2i? TargetTile;
 }
 
