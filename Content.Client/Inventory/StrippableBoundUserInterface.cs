c7685f4094c641af49d84a928cdc41fb81a889db
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2024-09-27 17:12:10 +1000

    Predict stripping (#32478)
    * Predict stripping
    
    Stops mob verbs from getting moved around again.
    
    * Bola
    
    * Fix ftl

--- Content.Client/Inventory/StrippableBoundUserInterface.cs
+++ Content.Client/Inventory/StrippableBoundUserInterface.cs
@@ -142,7 +142,7 @@ namespace Content.Client.Inventory
                     StyleClasses = { StyleBase.ButtonOpenRight }
                 };
 
-                button.OnPressed += (_) => SendMessage(new StrippingEnsnareButtonPressed());
+                button.OnPressed += (_) => SendPredictedMessage(new StrippingEnsnareButtonPressed());
 
                 _strippingMenu.SnareContainer.AddChild(button);
             }
@@ -182,7 +182,7 @@ namespace Content.Client.Inventory
             // So for now: only stripping & examining
             if (ev.Function == EngineKeyFunctions.Use)
             {
-                SendMessage(new StrippingSlotButtonPressed(slot.SlotName, slot is HandButton));
+                SendPredictedMessage(new StrippingSlotButtonPressed(slot.SlotName, slot is HandButton));
                 return;
             }
 
