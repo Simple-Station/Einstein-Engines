6cb456270737db05e6b8d8db0865ba1f18867fa0
VMSolidus <evilexecutive@gmail.com>
2024-10-05 10:28:46 -0400

    MoodSystem Crit Threshold CVar (#1010)
    # Description
    
    This PR adds a CVar that allows server hosts (such as N14 who need it)
    to optionally disable the Crit Threshold modification part of the
    MoodSystem. This is useful if a server has some other system that
    frequently messes with Thresholds, and that a server host wishes their
    system to not have to fight with Mood for it.
    
    # Changelog
    
    No changelog because this isn't playerfacing.

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -2461,6 +2461,9 @@ namespace Content.Shared.CCVar
         public static readonly CVarDef<bool> MoodDecreasesSpeed =
             CVarDef.Create("mood.decreases_speed", true, CVar.SERVER);
 
+        public static readonly CVarDef<bool> MoodModifiesThresholds =
+            CVarDef.Create("mood.modify_thresholds", false, CVar.SERVER);
+
         #endregion
 
         #region Lying Down System
