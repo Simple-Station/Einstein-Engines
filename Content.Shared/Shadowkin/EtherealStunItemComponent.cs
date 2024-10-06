3fe5842774a7df6560b0ab99ccf13fa1d071cfa8
FoxxoTrystan <trystan.garnierhein@gmail.com>
2024-10-06 10:44:45 +0200

    Bluespace Crystal

---
+++ Content.Shared/Shadowkin/EtherealStunItemComponent.cs
@@ -0,0 +1,15 @@
+namespace Content.Shared.Shadowkin
+{
+    [RegisterComponent]
+    public sealed partial class EtherealStunItemComponent : Component
+    {
+        [DataField]
+        public float Radius = 10;
+
+        [DataField]
+        public float ManaDamage = 50;
+
+        [DataField]
+        public bool DeleteOnUse = true;
+    }
+}
