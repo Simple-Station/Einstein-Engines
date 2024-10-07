c68f9e387b5c6f9a2d97767dbd053ef5fb562ed7
VMSolidus <evilexecutive@gmail.com>
2024-10-06 22:37:23 -0400

    Update MechGunSystem.cs

--- Content.Server/Mech/Equipment/EntitySystems/MechGunSystem.cs
+++ Content.Server/Mech/Equipment/EntitySystems/MechGunSystem.cs
@@ -34,15 +34,11 @@ public sealed class MechGunSystem : EntitySystem
             || !TryComp<MechComponent>(mechEquipment.EquipmentOwner.Value, out var mech))
             return;
 
-        var maxCharge = component.MaxCharge;
-        var currentCharge = component.CurrentCharge;
-        var chargeDelta = maxCharge - currentCharge;
-
+        var chargeDelta = component.MaxCharge - component.CurrentCharge;
         // TODO: The battery charge of the mech would be spent directly when fired.
-        if (chargeDelta <= 0 || mech.Energy - chargeDelta < 0)
-            return;
-
-        if (!_mech.TryChangeEnergy(mechEquipment.EquipmentOwner.Value, -chargeDelta, mech))
+        if (chargeDelta <= 0
+            || mech.Energy - chargeDelta < 0
+            || !_mech.TryChangeEnergy(mechEquipment.EquipmentOwner.Value, -chargeDelta, mech))
             return;
 
         _battery.SetCharge(uid, component.MaxCharge, component);
