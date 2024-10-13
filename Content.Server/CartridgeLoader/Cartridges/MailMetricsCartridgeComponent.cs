6a815ba315e87b5337afcf1966d66748c0236b0f
Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>
2024-10-13 22:38:19 +0300

    Feat: Port All Delta-V Mail Improvements (#1011)
    # Description
    Showing some love to our hard-working couriers.
    
    This cherry-picks the following PRs from delta-v:
    - https://github.com/DeltaV-Station/Delta-v/pull/1472 - Adds a
    MailMetrics PDA cartridge for couriers and the LO
    - https://github.com/DeltaV-Station/Delta-v/pull/1652 - Ports mail
    tweaks from frontier to delta-v, adding large packages, RPDs (mail
    cannons), and more mail
    - https://github.com/DeltaV-Station/Delta-v/pull/1788 - Adding a couple
    more packages
    - https://github.com/DeltaV-Station/Delta-v/pull/1925 - Fixing the
    non-functional "last known location" part of mail descriptions
    
    Some mail items, such as the opporozidone syringe and rainbow
    joints/blunts, had to be disabled because we don't have them (yet?)
    
    <details><summary><h1>Media</h1></summary>
    <p>
    
    
    ![image](https://github.com/user-attachments/assets/2299ffc3-17ce-4743-886c-7705e8789e96)
    
    
    ![image](https://github.com/user-attachments/assets/a2ee1a07-d765-41e7-8f70-f94a50412398)
    
    
    ![image](https://github.com/user-attachments/assets/03b16b8e-0eff-4332-99ad-d4a201ee8f2b)
    
    
    ![image](https://github.com/user-attachments/assets/44d2e5c2-db79-4d8d-8877-693648edd35d)
    
    
    ![image](https://github.com/user-attachments/assets/f3d7bdbb-fc14-4f2f-bf30-aa54fdf49099)
    
    
    ![image](https://github.com/user-attachments/assets/b1e4c53f-c9d8-496b-a72e-f4f4b702e368)
    
    </p>
    </details>
    
    ---
    
    # Changelog
    :cl:
    - add: The Courier and Logistics Officer now have a new program in their
    PDA for tracking mail delivery performance, including earnings and
    percent of packages opened, damaged, or expired.
    - add: The list of possible mail packages has been greately expanded,
    and now includes large parcels.
    - add: The CourierDrobe now offers a rapid mail delivery device, along
    with capsules for it.
    
    ---------
    
    Signed-off-by: Adeinitas <147965189+adeinitas@users.noreply.github.com>
    Co-authored-by: portfiend <109661617+portfiend@users.noreply.github.com>
    Co-authored-by: byte <50130120+huckleton@users.noreply.github.com>
    Co-authored-by: deltanedas <39013340+deltanedas@users.noreply.github.com>
    Co-authored-by: Adeinitas <147965189+adeinitas@users.noreply.github.com>
    Co-authored-by: ErhardSteinhauer <65374927+ErhardSteinhauer@users.noreply.github.com>
    Co-authored-by: Dvir <dvirf01@gmail.com>
    Co-authored-by: Dvir <39403717+dvir001@users.noreply.github.com>
    Co-authored-by: Whatstone <166147148+whatston3@users.noreply.github.com>
    Co-authored-by: Whatstone <whatstone3@gmail.com>
    Co-authored-by: Danger Revolution! <142105406+DangerRevolution@users.noreply.github.com>
    Co-authored-by: Milon <plmilonpl@gmail.com>

---
+++ Content.Server/CartridgeLoader/Cartridges/MailMetricsCartridgeComponent.cs
@@ -0,0 +1,11 @@
+namespace Content.Server.CartridgeLoader.Cartridges;
+
+[RegisterComponent, Access(typeof(MailMetricsCartridgeSystem))]
+public sealed partial class MailMetricsCartridgeComponent : Component
+{
+    /// <summary>
+    /// Station entity keeping track of logistics stats
+    /// </summary>
+    [DataField]
+    public EntityUid? Station;
+}
