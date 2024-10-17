e445a95953d4f84c53d284b8e821162a2b8613be
Thomas <87614336+aeshus@users.noreply.github.com>
2024-10-17 17:01:32 +0300

    Sanitize shorthand emotes throughought the whole message (#28645)
    * Rename ChatSanitizationManager to ChatEmoteSanitizationManager
    
    The prior name was kind of confusing as there's a emote one and
    then now there's also chat expansion happening in the accent system,
    so knowing which I actually need to edit is useful.
    
    So, I just need to keep myself not confused.
    
    * Rename smileyToEmote and remove punctuation duplicates
    
    The name SmileyToEmote is just... Bad.
    
    Plus, I needed to remove the punctuation duplicates as that would
    break any kind of regex parsing that I tried.
    
    * Switch to regex from checking end of string
    
    I also changed from System.Globalization to ILocalizationManager.
    
    Writing that regex was definitely an experience.
    
    * Document regex and the manager
    
    * Rename it back
    
    * Simplify regex
    
    (cherry picked from commit 876c44cd660f729aa9c7b0ab3b1d4cad586afd20)

--- Content.Server/Chat/Systems/ChatSystem.cs
+++ Content.Server/Chat/Systems/ChatSystem.cs
@@ -816,7 +816,9 @@ public sealed partial class ChatSystem : SharedChatSystem
         var newMessage = SanitizeMessageReplaceWords(message.Trim());
 
         GetRadioKeycodePrefix(source, newMessage, out newMessage, out var prefix);
-        _sanitizer.TrySanitizeOutSmilies(newMessage, source, out newMessage, out emoteStr);
+
+        // Sanitize it first as it might change the word order
+        _sanitizer.TrySanitizeEmoteShorthands(newMessage, source, out newMessage, out emoteStr);
 
         if (capitalize)
             newMessage = SanitizeMessageCapital(newMessage);
