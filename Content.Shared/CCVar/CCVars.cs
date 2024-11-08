89b0806c56c5bd7401541c683a6ebd82c0bcb3d0
DEATHB4DEFEAT <zachcaffee@outlook.com>
2024-11-08 13:36:27 -0800

    b

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -504,6 +504,7 @@ namespace Content.Shared.CCVar
             CVarDef.Create("tts.enabled", false, CVar.SERVERONLY);
 
         /// Can be "file" to store in the cache_path, or "memory" to store it in memory.
+        /// Memory is way faster, but servers are usually more limited by memory than storage, pick your poison.
         public static readonly CVarDef<string> TTSCacheType =
             CVarDef.Create("tts.cache_type", "memory", CVar.SERVERONLY);
 
