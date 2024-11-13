2c1dcd2fc31fd1d5c19972d7e1992c8e9a946693
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2024-11-12 22:46:45 -0400

    Update Content.Shared/CCVar/CCVars.cs
    Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>
    Signed-off-by: sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -2645,21 +2645,17 @@ namespace Content.Shared.CCVar
         * AUTOVOTE SYSTEM
         */
 
-        /// <summary>
-        ///     Enables the automatic voting system.
-        /// </summary>
+        /// Enables the automatic voting system.
         public static readonly CVarDef<bool> AutoVoteEnabled =
             CVarDef.Create("vote.autovote_enabled", false, CVar.SERVERONLY);
 
-        /// <summary>
-        ///     Automatically make map votes on return to lobby? Requires auto voting to be enabled.
-        /// </summary>      
+        /// Automatically starts a map vote when returning to the lobby.
+        /// Requires auto voting to be enabled.  
         public static readonly CVarDef<bool> MapAutoVoteEnabled =
             CVarDef.Create("vote.map_autovote_enabled", true, CVar.SERVERONLY);
 
-        /// <summary>
-        ///     Automatically make preset votes on return to lobby? Requires auto voting to be enabled.
-        /// </summary>      
+        /// Automatically starts a gamemode vote when returning to the lobby.
+        /// Requires auto voting to be enabled.  
         public static readonly CVarDef<bool> PresetAutoVoteEnabled =
             CVarDef.Create("vote.preset_autovote_enabled", true, CVar.SERVERONLY);
     }
