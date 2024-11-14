387a362ae9658b60f72db390b420998aea7bf8b0
Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>
2024-11-14 23:03:33 +0300

    Some Language Fixes (#1223)
    # Description
    1. Adds languages to... penguins, cockroaches, snakes, mmis, pdas, mail
    packages. Positronic brains now only speak/understand the borg language.
    2. Adds an explicitly defined universal language to revenants and wisps
    3. Makes polymorphs copy over a list of components defined in the
    polymorph proto, thus copying languages and grammar of the original
    entity to its polymorphed version.
    
    <details><summary><h1>Media</h1></summary>
    <p>
    
    
    ![image](https://github.com/user-attachments/assets/111c3f91-f077-431f-bcd9-f60fa7a1ba26)
    
    
    ![image](https://github.com/user-attachments/assets/80f939d3-b615-4235-a511-82238758788e)
    
    
    ![image](https://github.com/user-attachments/assets/dce99b55-c8b1-4cbe-8769-b82151b3d404)
    
    
    ![image](https://github.com/user-attachments/assets/6b9b962d-c73c-42b9-b240-aa07601de879)
    
    
    
    </p>
    </details>
    
    # Changelog
    :cl:
    - tweak: Added languages to certain entities that lacked them, including
    MMIs and positronic brains.
    - add: Polymorphing into another entity now preserves your languages and
    grammar.

--- Content.Server/Polymorph/Systems/PolymorphSystem.cs
+++ Content.Server/Polymorph/Systems/PolymorphSystem.cs
@@ -20,6 +20,7 @@ using Robust.Server.Containers;
 using Robust.Server.GameObjects;
 using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.Manager;
 using Robust.Shared.Timing;
 using Robust.Shared.Utility;
 
@@ -31,6 +32,7 @@ public sealed partial class PolymorphSystem : EntitySystem
     [Dependency] private readonly IMapManager _mapManager = default!;
     [Dependency] private readonly IPrototypeManager _proto = default!;
     [Dependency] private readonly IGameTiming _gameTiming = default!;
+    [Dependency] private readonly ISerializationManager _serialization = default!;
     [Dependency] private readonly ActionsSystem _actions = default!;
     [Dependency] private readonly AudioSystem _audio = default!;
     [Dependency] private readonly SharedBuckleSystem _buckle = default!;
@@ -201,6 +203,19 @@ public sealed partial class PolymorphSystem : EntitySystem
 
         var child = Spawn(configuration.Entity, _transform.GetMapCoordinates(uid, targetTransformComp), rotation: _transform.GetWorldRotation(uid));
 
+        // Copy specified components over
+        foreach (var compName in configuration.CopiedComponents)
+        {
+            if (!_compFact.TryGetRegistration(compName, out var reg)
+                || !EntityManager.TryGetComponent(uid, reg.Idx, out var comp))
+                continue;
+
+            var copy = _serialization.CreateCopy(comp, notNullableOverride: true);
+            copy.Owner = child;
+            AddComp(child, copy, true);
+        }
+
+        // Ensure the resulting entity is sentient (why? this sucks)
         MakeSentientCommand.MakeSentient(child, EntityManager);
 
         var polymorphedComp = _compFact.GetComponent<PolymorphedEntityComponent>();
