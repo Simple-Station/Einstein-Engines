7389c94e274556c5cbc321812ceccb270636afed
sleepyyapril <flyingkarii@gmail.com>
2024-11-15 19:58:39 -0400

    Solidus input!

--- Content.Shared/Standing/StandingStateSystem.cs
+++ Content.Shared/Standing/StandingStateSystem.cs
@@ -59,7 +59,7 @@ public sealed class StandingStateSystem : EntitySystem
         if (dropHeldItems && hands != null)
             RaiseLocalEvent(uid, new DropHandItemsEvent(), false);
 
-        if (TryComp(uid, out BuckleComponent? buckle) && buckle.Buckled && !_buckle.TryUnbuckle(uid, uid, buckleComp: buckle))
+        if (TryComp(uid, out BuckleComponent? buckle) && buckle.Buckled)
             return false;
 
         if (!force)
