1146c5ba42c38e677cfb5bae12eab306d72977a2
VMSolidus <evilexecutive@gmail.com>
2024-11-21 20:07:42 -0500

    Engine Shennanigans

--- Content.Server/WhiteDream/BloodCult/Gamerule/BloodCultRuleComponent.cs
+++ Content.Server/WhiteDream/BloodCult/Gamerule/BloodCultRuleComponent.cs
@@ -19,7 +19,7 @@ public sealed partial class BloodCultRuleComponent : Component
     public ProtoId<NpcFactionPrototype> BloodCultFaction = "GeometerOfBlood";
 
     [DataField]
-    public ProtoId<EntityPrototype> HarvesterPrototype = "ConstructHarvester";
+    public EntProtoId HarvesterPrototype = "ConstructHarvester";
 
     [DataField]
     public Color EyeColor = Color.FromHex("#f80000");
