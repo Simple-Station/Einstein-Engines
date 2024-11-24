34540e5cd35f8ab29ad3e5f47fb7da7f44fe4367
Eris <erisfiregamer1@gmail.com>
2024-11-24 18:18:57 -0500

    webedit jumpscare 2
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    
    Signed-off-by: Eris <erisfiregamer1@gmail.com>

--- Content.Shared/Nyanotrasen/Kitchen/SharedDeepfryerSystem.cs
+++ Content.Shared/Nyanotrasen/Kitchen/SharedDeepfryerSystem.cs
@@ -1,5 +1,37 @@
-﻿namespace Content.Shared.Nyanotrasen.Kitchen;
+using Content.Shared.DragDrop;
+using Robust.Shared.Physics.Components;
+using Robust.Shared.Physics.Events;
+using Robust.Shared.Physics.Systems;
+using Content.Shared.Nyanotrasen.Kitchen.Components;
+using Content.Shared.Item;
+using Content.Shared.Body.Components;
+
+namespace Content.Shared.Nyanotrasen.Kitchen;
 
 public abstract class SharedDeepfryerSystem : EntitySystem
 {
+    protected void OnCanDragDropOn(EntityUid uid, SharedDeepFryerComponent component, ref CanDropTargetEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        args.CanDrop = CanInsert(uid, args.Dragged);
+        args.Handled = true;
+    }
+
+    public virtual bool CanInsert(EntityUid uid, EntityUid entity)
+    {
+        if (!Transform(uid).Anchored)
+            return false;
+
+        var storable = HasComp<ItemComponent>(entity);
+        if (!storable && !HasComp<BodyComponent>(entity))
+            return false;
+
+        if (TryComp<PhysicsComponent>(entity, out var physics) && (physics.CanCollide) || storable)
+            return true;
+        else
+            return false;
+
+    }
 }
