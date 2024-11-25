97ce1c2c3eacc668a6c4f287c336941f89c7bbde
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2024-11-25 07:20:31 +0000

    "Ghost" in the lobby lets you see the whole chat (#33529)
    * fix bug, in ghost command lobby
    
    * fix
    
    * Fix build

--- Content.Server/Ghost/GhostCommand.cs
+++ Content.Server/Ghost/GhostCommand.cs
@@ -1,8 +1,10 @@
 using Content.Server.GameTicking;
 using Content.Server.Popups;
 using Content.Shared.Administration;
+using Content.Shared.GameTicking;
 using Content.Shared.Mind;
 using Robust.Shared.Console;
+using Content.Server.GameTicking;
 
 namespace Content.Server.Ghost
 {
@@ -21,6 +23,14 @@ namespace Content.Server.Ghost
             if (player == null)
             {
                 shell.WriteLine(Loc.GetString("ghost-command-no-session"));
+                return;
+            }
+
+            var gameTicker = _entities.System<GameTicker>();
+            if (!gameTicker.PlayerGameStatuses.TryGetValue(player.UserId, out var playerStatus) ||
+                playerStatus is not PlayerGameStatus.JoinedGame)
+            {
+                shell.WriteLine("ghost-command-error-lobby");
                 return;
             }
 
