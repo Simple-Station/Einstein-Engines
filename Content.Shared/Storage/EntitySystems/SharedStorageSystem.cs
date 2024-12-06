d69b516ce2fe3f8da65db745f1157ae259f60e96
VMSolidus <evilexecutive@gmail.com>
2024-12-05 22:13:08 -0500

    Port Shipyards (#1314)
    # Description
    
    I needed this extremely badly.
    
    # TODO
    
    - [x] Move all of this out of the DeltaV folders.
    
    # Changelog
    
    :cl:
    - add: Added Shipyards.
    
    ---------
    
    Signed-off-by: deltanedas <39013340+deltanedas@users.noreply.github.com>
    Co-authored-by: deltanedas <39013340+deltanedas@users.noreply.github.com>
    Co-authored-by: Null <56081759+NullWanderer@users.noreply.github.com>
    Co-authored-by: Milon <milonpl.git@proton.me>
    Co-authored-by: Plykiya <58439124+plykiya@users.noreply.github.com>
    Co-authored-by: plykiya <plykiya@protonmail.com>
    Co-authored-by: Ed <96445749+TheShuEd@users.noreply.github.com>
    Co-authored-by: Radezolid <snappednexus@gmail.com>
    Co-authored-by: sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>

--- Content.Shared/Storage/EntitySystems/SharedStorageSystem.cs
+++ Content.Shared/Storage/EntitySystems/SharedStorageSystem.cs
@@ -24,6 +24,7 @@ using Content.Shared.Stacks;
 using Content.Shared.Storage.Components;
 using Content.Shared.Timing;
 using Content.Shared.Verbs;
+using Content.Shared.Whitelist;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Containers;
 using Robust.Shared.GameStates;
@@ -58,6 +59,7 @@ public abstract class SharedStorageSystem : EntitySystem
     [Dependency] protected readonly SharedTransformSystem TransformSystem = default!;
     [Dependency] private   readonly SharedUserInterfaceSystem _ui = default!;
     [Dependency] protected readonly UseDelaySystem UseDelay = default!;
+    [Dependency] private readonly EntityWhitelistSystem _whitelistSystem = default!;
 
     private EntityQuery<ItemComponent> _itemQuery;
     private EntityQuery<StackComponent> _stackQuery;
@@ -864,13 +866,8 @@ public abstract class SharedStorageSystem : EntitySystem
             return false;
         }
 
-        if (storageComp.Whitelist?.IsValid(insertEnt, EntityManager) == false)
-        {
-            reason = "comp-storage-invalid-container";
-            return false;
-        }
-
-        if (storageComp.Blacklist?.IsValid(insertEnt, EntityManager) == true)
+        if (_whitelistSystem.IsWhitelistFail(storageComp.Whitelist, insertEnt) ||
+            _whitelistSystem.IsBlacklistPass(storageComp.Blacklist, insertEnt))
         {
             reason = "comp-storage-invalid-container";
             return false;
