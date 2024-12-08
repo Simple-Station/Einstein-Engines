892db4a806acec13e345cb40aaccaead5ff33e93
sleepyyapril <flyingkarii@gmail.com>
2024-12-08 00:42:42 -0400

    Moved to Misc for anything that isn't truly a character requirement, but still needs to inherit as such.

---
+++ Content.Shared/Customization/Systems/CharacterRequirements.Misc.cs
@@ -0,0 +1,45 @@
+/// <summary>
+///     Requires the profile to have one of the specified loadouts
+/// </summary>
+[UsedImplicitly, Serializable, NetSerializable,]
+public sealed partial class CVarRequirement : CharacterRequirement
+{
+    [DataField("cvar", required: true)]
+    public string CVar;
+
+    [DataField("requiredValue", required: true)]
+    public string RequiredValue;
+
+    public override bool IsValid(
+        JobPrototype job,
+        HumanoidCharacterProfile profile,
+        Dictionary<string, TimeSpan> playTimes,
+        bool whitelisted,
+        IPrototype prototype,
+        IEntityManager entityManager,
+        IPrototypeManager prototypeManager,
+        IConfigurationManager configManager,
+        out string? reason,
+        int depth = 0
+    )
+    {
+        if (!configManager.IsCVarRegistered(CVar))
+        {
+            reason = null;
+            return true;
+        }
+
+        const string color = "lightblue";
+        var cvar = configManager.GetCVar(CVar);
+        var isValid = cvar.ToString()! == RequiredValue;
+
+        reason = Loc.GetString(
+            "character-cvar-requirement",
+            ("inverted", Inverted),
+            ("color", color),
+            ("cvar", CVar),
+            ("value", RequiredValue));
+
+        return isValid;
+    }
+}
