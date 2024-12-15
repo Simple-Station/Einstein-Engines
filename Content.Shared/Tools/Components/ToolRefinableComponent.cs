RebelledCombine/Zona-14

Welding tweaks (#27959)

Content.Shared/Tools/Components/ToolRefinableComponent.cs
@@ -1,15 +1,16 @@
 using Content.Shared.Storage;
-using Content.Shared.Tools;
+using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
+using Content.Shared.Tools.Systems;
 
-namespace Content.Server.Construction.Components;
+namespace Content.Shared.Tools.Components;
 
 /// <summary>
 /// Used for something that can be refined by welder.
 /// For example, glass shard can be refined to glass sheet.
 /// </summary>
-[RegisterComponent, Access(typeof(RefiningSystem))]
-public sealed partial class WelderRefinableComponent : Component
+[RegisterComponent, NetworkedComponent, Access(typeof(ToolRefinablSystem))]
+public sealed partial class ToolRefinableComponent : Component
 {
     /// <summary>
     /// The items created when the item is refined.
@@ -27,7 +28,7 @@ public sealed partial class WelderRefinableComponent : Component
     /// The amount of fuel it takes to refine a given item.
     /// </summary>
     [DataField]
-    public float RefineFuel;
+    public float RefineFuel = 3f;
 
     /// <summary>
     /// The tool type needed in order to refine this item.

Simple-Station/Einstein-Engines

Content.Shared/Tools/Components/ToolRefinableComponent.cs
@@ -1,15 +1,17 @@
 using Content.Shared.Tools;
 using Content.Shared.Storage;
+using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
+using Content.Shared.Tools.Systems;
 
-namespace Content.Server.Construction.Components;
+namespace Content.Shared.Tools.Components;
 
 /// <summary>
 /// Used for something that can be refined by welder.
 /// For example, glass shard can be refined to glass sheet.
 /// </summary>
-[RegisterComponent]
-public sealed partial class WelderRefinableComponent : Component
+[RegisterComponent, NetworkedComponent, Access(typeof(ToolRefinableSystem))]
+public sealed partial class ToolRefinableComponent : Component
 {
     [DataField]
     public HashSet<EntitySpawnEntry>? RefineResult;
@@ -18,7 +20,7 @@ public sealed partial class WelderRefinableComponent : Component
     public float RefineTime = 2f;
 
     [DataField]
-    public float RefineFuel;
+    public float RefineFuel = 3f;
 
     [DataField]
     public ProtoId<ToolQualityPrototype> QualityNeeded = "Welding";