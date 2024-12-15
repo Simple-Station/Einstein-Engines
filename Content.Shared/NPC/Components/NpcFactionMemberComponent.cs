0b0bfa8f00345706a7592521163eacf067ec9195
sleepyyapril <flyingkarii@gmail.com>
2024-12-15 13:38:01 -0400

    yeag

--- Content.Shared/NPC/Components/NpcFactionMemberComponent.cs
+++ Content.Shared/NPC/Components/NpcFactionMemberComponent.cs
@@ -25,4 +25,12 @@ public sealed partial class NpcFactionMemberComponent : Component
     /// </summary>
     [ViewVariables]
     public readonly HashSet<ProtoId<NpcFactionPrototype>> HostileFactions = new();
+
+    // Nyano - Summary - Begin modified code block: support for specific entities to be friendly.
+    /// <summary>
+    /// Permanently friendly specific entities. Our summoner, etc.
+    /// Would like to separate. Could I do that by extending this method, maybe?
+    /// </summary>
+    public HashSet<EntityUid> ExceptionalFriendlies = new();
+    // Nyano - End modified code block.
 }
