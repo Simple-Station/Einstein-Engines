c83362263ff720c4f8819a8ba216c3efb94eceee
sleepyyapril <flyingkarii@gmail.com>
2024-12-19 13:11:29 -0400

    Revert "the tests are only running online so ill just push this here"
    This reverts commit b7d3d7147796c3bad21efa33447937e45ab4e55e.

--- Content.Shared/Chat/SharedSuicideSystem.cs
+++ Content.Shared/Chat/SharedSuicideSystem.cs
@@ -10,16 +10,6 @@ public sealed class SharedSuicideSystem : EntitySystem
 {
     [Dependency] private readonly DamageableSystem _damageableSystem = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
-    [Dependency] private readonly ILogManager _logManager = default!;
-
-    private ISawmill _sawmill = default!;
-
-    public override void Initialize()
-    {
-        base.Initialize();
-
-        _sawmill = _logManager.GetSawmill("suicide");
-    }
 
     /// <summary>
     /// Applies lethal damage spread out across the damage types given.
@@ -29,23 +19,14 @@ public sealed class SharedSuicideSystem : EntitySystem
         // Create a new damageSpecifier so that we don't make alterations to the original DamageSpecifier
         // Failing  to do this will permanently change a weapon's damage making it insta-kill people
         var appliedDamageSpecifier = new DamageSpecifier(damageSpecifier);
-        var damagesStr = "";
-
-        foreach (var damage in appliedDamageSpecifier.DamageDict)
-            damagesStr += damage.Key + ": " + damage.Value + "\n";
-
-        _sawmill.Info($"Applying lethal damage (specifier) 1: \nnum damage types: {appliedDamageSpecifier.DamageDict.Count}\ndamages:\n{damagesStr}");
         if (!TryComp<MobThresholdsComponent>(target, out var mobThresholds))
             return;
-
-        _sawmill.Info("Applying lethal damage (specifier) 2");
 
         // Mob thresholds are sorted from alive -> crit -> dead,
         // grabbing the last key will give us how much damage is needed to kill a target from zero
         // The exact lethal damage amount is adjusted based on their current damage taken
         var lethalAmountOfDamage = mobThresholds.Thresholds.Keys.Last() - target.Comp.TotalDamage;
         var totalDamage = appliedDamageSpecifier.GetTotal();
-        _sawmill.Info("Applying lethal damage (specifier) 3");
 
         // Removing structural because it causes issues against entities that cannot take structural damage,
         // then getting the total to use in calculations for spreading out damage.
@@ -55,8 +36,6 @@ public sealed class SharedSuicideSystem : EntitySystem
         foreach (var (key, value) in appliedDamageSpecifier.DamageDict)
             appliedDamageSpecifier.DamageDict[key] = Math.Ceiling((double) (value * lethalAmountOfDamage / totalDamage));
 
-        _sawmill.Info("Applying lethal damage (specifier): " + appliedDamageSpecifier.GetTotal());
-
         _damageableSystem.TryChangeDamage(target, appliedDamageSpecifier, true, origin: target);
     }
 
@@ -65,17 +44,13 @@ public sealed class SharedSuicideSystem : EntitySystem
     /// </summary>
     public void ApplyLethalDamage(Entity<DamageableComponent> target, ProtoId<DamageTypePrototype>? damageType)
     {
-        _sawmill.Info("Applying lethal damage (singular) 1");
         if (!TryComp<MobThresholdsComponent>(target, out var mobThresholds))
             return;
-
-        _sawmill.Info("Applying lethal damage (singular) 2");
 
         // Mob thresholds are sorted from alive -> crit -> dead,
         // grabbing the last key will give us how much damage is needed to kill a target from zero
         // The exact lethal damage amount is adjusted based on their current damage taken
         var lethalAmountOfDamage = mobThresholds.Thresholds.Keys.Last() - target.Comp.TotalDamage;
-        _sawmill.Info("Applying lethal damage (singular): " + lethalAmountOfDamage);
 
         // We don't want structural damage for the same reasons listed above
         if (!_prototypeManager.TryIndex(damageType, out var damagePrototype) || damagePrototype.ID == "Structural")
@@ -85,7 +60,6 @@ public sealed class SharedSuicideSystem : EntitySystem
         }
 
         var damage = new DamageSpecifier(damagePrototype, lethalAmountOfDamage);
-        _sawmill.Info("Applying lethal damage (singular): " + damage.GetTotal());
         _damageableSystem.TryChangeDamage(target, damage, true, origin: target);
     }
 }
