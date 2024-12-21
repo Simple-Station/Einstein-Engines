d49e8f6fc0081d1d73c21feacd3439840bfd7fdd
sleepyyapril <flyingkarii@gmail.com>
2024-12-21 16:02:33 -0400

    SyndieBorgs can laugh again.

--- Content.Server/Chat/Systems/ChatSystem.Emote.cs
+++ Content.Server/Chat/Systems/ChatSystem.Emote.cs
@@ -186,12 +186,18 @@ public partial class ChatSystem
     /// <returns></returns>
     private bool AllowedToUseEmote(EntityUid source, EmotePrototype emote)
     {
-        if ((_whitelistSystem.IsWhitelistFail(emote.Whitelist, source) || _whitelistSystem.IsBlacklistPass(emote.Blacklist, source)))
+        // If emote is in AllowedEmotes, it will bypass whitelist and blacklist
+        if (TryComp<SpeechComponent>(source, out var speech) &&
+            speech.AllowedEmotes.Contains(emote.ID))
+            return true;
+
+        // Check the whitelist and blacklist
+        if (_whitelistSystem.IsWhitelistFail(emote.Whitelist, source) ||
+            _whitelistSystem.IsBlacklistPass(emote.Blacklist, source))
             return false;
 
-        if (!emote.Available &&
-            TryComp<SpeechComponent>(source, out var speech) &&
-            !speech.AllowedEmotes.Contains(emote.ID))
+        // Check if the emote is available for all
+        if (!emote.Available)
             return false;
 
         return true;
