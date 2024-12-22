143c6a6a9c099e335300d6358181dcb3ceb6bef4
Raphael Bertoche <bertocheraphael@gmail.com>
2024-12-22 04:30:09 -0300

    Adds methods TryGetItems and Where to AntagSelectionPlayerPool

--- Content.Server/Antag/AntagSelectionPlayerPool.cs
+++ Content.Server/Antag/AntagSelectionPlayerPool.cs
@@ -23,5 +23,44 @@ public sealed class AntagSelectionPlayerPool (List<List<ICommonSession>> ordered
         return session != null;
     }
 
+    // EE
+    public bool TryGetItems(IRobustRandom random,
+                            [NotNullWhen(true)] out ICommonSession[]? sessions,
+                            int count,
+                            bool allowDuplicates = true)
+    {
+        sessions = null;
+        List<ICommonSession> session_list = [];
+
+        foreach (var pool in orderedPools)
+        {
+            if (pool.Count == 0)
+                continue;
+
+            var picked = random.GetItems(pool, count - session_list.Count, allowDuplicates);
+            session_list.AddRange(picked);
+            if (session_list.Count < count)
+            {
+                continue;
+            }
+            sessions = session_list.ToArray();
+            break;
+        }
+
+        return sessions != null;
+    }
+
+    // EE
+    public AntagSelectionPlayerPool Where(Func<ICommonSession, bool> predicate)
+    {
+        var newPools = orderedPools.Select(
+            (pool) =>
+            {
+                return pool.Where(predicate).ToList();
+            }
+        );
+
+        return new AntagSelectionPlayerPool(newPools.ToList());
+    }
     public int Count => orderedPools.Sum(p => p.Count);
 }
