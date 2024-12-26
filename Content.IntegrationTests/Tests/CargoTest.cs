91ca7e3fb95622a9672e0a97ef81b2dd052d8798
sleepyyapril <flyingkarii@gmail.com>
2024-12-26 19:30:02 -0400

    Update message for failing to spawn.

--- Content.IntegrationTests/Tests/CargoTest.cs
+++ Content.IntegrationTests/Tests/CargoTest.cs
@@ -132,7 +132,7 @@ public sealed class CargoTest
                 }
                 catch (Exception e)
                 {
-                    Assert.Fail(proto);
+                    Assert.Fail($"Prototype {proto} failed to spawn! Is your configuration invalid?");
                     return;
                 }
 
