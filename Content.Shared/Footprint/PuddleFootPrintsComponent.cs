983958c14eb3257027d9b55d439c22a3dd874dbc
VMSolidus <evilexecutive@gmail.com>
2025-01-05 12:47:19 -0500

    More Puddle Fixes

--- Content.Shared/Footprint/PuddleFootPrintsComponent.cs
+++ Content.Shared/Footprint/PuddleFootPrintsComponent.cs
@@ -1,11 +1,15 @@
+using Content.Shared.FixedPoint;
+
+
 namespace Content.Shared.FootPrint;
 
+// Floof: this system has been effectively rewriteen. DO NOT MERGE UPSTREAM CHANGES.
 [RegisterComponent]
 public sealed partial class PuddleFootPrintsComponent : Component
 {
+    /// <summary>
+    ///     Ratio between puddle volume and the amount of reagents that can be transferred from it.
+    /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
-    public float SizeRatio = 0.2f;
-
-    [ViewVariables(VVAccess.ReadWrite)]
-    public float OffPercent = 80f;
+    public FixedPoint2 SizeRatio = 0.15f;
 }
