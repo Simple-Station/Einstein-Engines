7285ad6c4664270c27c476d376406dbec068ffc2
sleepyyapril <flyingkarii@gmail.com>
2025-01-07 15:20:42 -0400

    yeah

--- Content.Server.Database/Migrations/Postgres/20241222203147_UpstreamMerge.cs
+++ Content.Server.Database/Migrations/Postgres/20241222203147_UpstreamMerge.cs
@@ -13,11 +13,7 @@ namespace Content.Server.Database.Migrations.Postgres
         {
             migrationBuilder.Sql("DROP TABLE IF EXISTS ProileLoadouts;");
 
-            migrationBuilder.AddColumn<DateTime>(
-                name: "last_read_rules",
-                table: "player",
-                type: "timestamp with time zone",
-                nullable: true);
+            migrationBuilder.Sql("ALTER TABLE \"player\" ADD IF NOT EXISTS \"last_read_rules\" TIMESTAMP WITH TIME ZONE NULL;");
 
             migrationBuilder.AddColumn<int>(
                 name: "hwid_type",
