b579a27c484ee30ffc94328867cb4edac294445e
sleepyyapril <flyingkarii@gmail.com>
2025-01-08 23:25:24 -0400

    remove FastAccessors

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -16,7 +16,6 @@ using Robust.Shared.GameObjects;
 using Robust.Shared.Map;
 using Robust.Shared.Map.Components;
 using Robust.Shared.Prototypes;
-using FastAccessors;
 using Content.Shared.Station.Components;
 using Robust.Shared.Utility;
 using YamlDotNet.RepresentationModel;
