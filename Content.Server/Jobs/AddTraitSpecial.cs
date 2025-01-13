bce5676ce4337b572b4206a7fc555e148f882af9
Peptide90 <User94636@protonmail.com>
2025-01-13 20:19:25 +0000

    simplify the code

--- Content.Server/Jobs/AddTraitSpecial.cs
+++ Content.Server/Jobs/AddTraitSpecial.cs
@@ -4,35 +4,27 @@ using Content.Shared.Traits;
 using JetBrains.Annotations;
 using Robust.Shared.Prototypes;
 
-namespace Content.Server.Jobs
+namespace Content.Server.Jobs;
+
+
+[UsedImplicitly]
+public sealed partial class AddTraitSpecial : JobSpecial
 {
-    [UsedImplicitly]
-    public sealed partial class AddTraitSpecial : JobSpecial
+    // Datafield for storing multiple trait prototype IDs as strings
+    [DataField(required: true)]
+    public HashSet<string> Traits { get; private set; } = new();
+
+    public override void AfterEquip(EntityUid mob)
     {
-        // Datafield for storing multiple trait prototype IDs as strings
-        [DataField("traits")]
-        public HashSet<string> Traits { get; private set; } = new();
+        // Resolve the necessary systems
+        var entityManager = IoCManager.Resolve<IEntityManager>();
+        var prototypeManager = IoCManager.Resolve<IPrototypeManager>();
+        var traitSystem = entityManager.System<TraitSystem>();
 
-        public override void AfterEquip(EntityUid mob)
-        {
-            if (Traits == null || Traits.Count == 0)
-                throw new InvalidOperationException("No traits are set.");
-
-            // Resolve the necessary systems
-            var entityManager = IoCManager.Resolve<IEntityManager>();
-            var prototypeManager = IoCManager.Resolve<IPrototypeManager>();
-            var traitSystem = entityManager.System<TraitSystem>();
-
-            // Iterate through each trait and add it to the entity
-            foreach (var traitId in Traits)
-            {
-                if (!prototypeManager.TryIndex(traitId, out TraitPrototype? traitPrototype))
-                {
-                    throw new InvalidOperationException($"Trait prototype '{traitId}' could not be found.");
-                }
+        // Iterate through each trait and add it to the entity
+        foreach (var traitId in Traits)
+            if (prototypeManager.TryIndex<TraitPrototype>(traitId, out var traitPrototype))
 
                 traitSystem.AddTrait(mob, traitPrototype);
-            }
-        }
     }
 }
