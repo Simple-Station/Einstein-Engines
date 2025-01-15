6350f2afe70f30689384ad8bba1ebc65c874a96f
SixplyDev <einlichen@gmail.com>
2025-01-14 21:37:06 -0600

    Re-Enable DarkSwap
    DarkSwap revamp will be its own PR, not for this PR. 
    
    Signed-off-by: SixplyDev <einlichen@gmail.com>

--- Content.Server/Abilities/Psionics/Abilities/DarkSwapSystem.cs
+++ Content.Server/Abilities/Psionics/Abilities/DarkSwapSystem.cs
@@ -1,58 +1,58 @@
-# using Content.Shared.Abilities.Psionics;
-# using Content.Shared.Actions.Events;
-# using Content.Shared.Shadowkin;
-# using Content.Shared.Physics;
-# using Content.Shared.Popups;
-# using Content.Shared.Maps;
-# using Robust.Server.GameObjects;
+using Content.Shared.Abilities.Psionics;
+using Content.Shared.Actions.Events;
+using Content.Shared.Shadowkin;
+using Content.Shared.Physics;
+using Content.Shared.Popups;
+using Content.Shared.Maps;
+using Robust.Server.GameObjects;
 
-# namespace Content.Server.Abilities.Psionics
-# {
-#     public sealed class DarkSwapSystem : EntitySystem
-#     {
-#         [Dependency] private readonly SharedPsionicAbilitiesSystem _psionics = default!;
-#         [Dependency] private readonly SharedPopupSystem _popup = default!;
-#         [Dependency] private readonly PhysicsSystem _physics = default!;
+namespace Content.Server.Abilities.Psionics
+{
+    public sealed class DarkSwapSystem : EntitySystem
+    {
+        [Dependency] private readonly SharedPsionicAbilitiesSystem _psionics = default!;
+        [Dependency] private readonly SharedPopupSystem _popup = default!;
+        [Dependency] private readonly PhysicsSystem _physics = default!;
 
-#         public override void Initialize()
-#         {
-#             base.Initialize();
-#             SubscribeLocalEvent<DarkSwapActionEvent>(OnPowerUsed);
-#         }
-# 
-#         private void OnPowerUsed(DarkSwapActionEvent args)
-#         {
-#             if (TryComp<EtherealComponent>(args.Performer, out var ethereal))
-#             {
-#                 var tileref = Transform(args.Performer).Coordinates.GetTileRef();
-#                 if (tileref != null
-#                 && _physics.GetEntitiesIntersectingBody(args.Performer, (int) CollisionGroup.Impassable).Count > 0)
-#                 {
-#                     _popup.PopupEntity(Loc.GetString("revenant-in-solid"), args.Performer, args.Performer);
-#                     return;
-#                 }
+        public override void Initialize()
+        {
+            base.Initialize();
+            SubscribeLocalEvent<DarkSwapActionEvent>(OnPowerUsed);
+        }
 
-#                 if (_psionics.OnAttemptPowerUse(args.Performer, "DarkSwap"))
-#                 {
-#                     SpawnAtPosition("ShadowkinShadow", Transform(args.Performer).Coordinates);
-#                     SpawnAtPosition("EffectFlashShadowkinDarkSwapOff", Transform(args.Performer).Coordinates);
-#                     RemComp(args.Performer, ethereal);
-#                     args.Handled = true;
-#                 }
-#             }
-#             else if (_psionics.OnAttemptPowerUse(args.Performer, "DarkSwap", args.ManaCost))
-#             {
-#                 var newethereal = EnsureComp<EtherealComponent>(args.Performer);
-#                 newethereal.Darken = true;
+        private void OnPowerUsed(DarkSwapActionEvent args)
+        {
+            if (TryComp<EtherealComponent>(args.Performer, out var ethereal))
+            {
+                var tileref = Transform(args.Performer).Coordinates.GetTileRef();
+                if (tileref != null
+                && _physics.GetEntitiesIntersectingBody(args.Performer, (int) CollisionGroup.Impassable).Count > 0)
+                {
+                    _popup.PopupEntity(Loc.GetString("revenant-in-solid"), args.Performer, args.Performer);
+                    return;
+                }
 
-#                 SpawnAtPosition("ShadowkinShadow", Transform(args.Performer).Coordinates);
-#                 SpawnAtPosition("EffectFlashShadowkinDarkSwapOn", Transform(args.Performer).Coordinates);
+                if (_psionics.OnAttemptPowerUse(args.Performer, "DarkSwap"))
+                {
+                    SpawnAtPosition("ShadowkinShadow", Transform(args.Performer).Coordinates);
+                    SpawnAtPosition("EffectFlashShadowkinDarkSwapOff", Transform(args.Performer).Coordinates);
+                    RemComp(args.Performer, ethereal);
+                    args.Handled = true;
+                }
+            }
+            else if (_psionics.OnAttemptPowerUse(args.Performer, "DarkSwap", args.ManaCost))
+            {
+                var newethereal = EnsureComp<EtherealComponent>(args.Performer);
+                newethereal.Darken = true;
 
-#                 args.Handled = true;
-#             }
+                SpawnAtPosition("ShadowkinShadow", Transform(args.Performer).Coordinates);
+                SpawnAtPosition("EffectFlashShadowkinDarkSwapOn", Transform(args.Performer).Coordinates);
 
-#             if (args.Handled)
-#                 _psionics.LogPowerUsed(args.Performer, "DarkSwap");
-#         }
-#     }
-# }
+                args.Handled = true;
+            }
+
+            if (args.Handled)
+                _psionics.LogPowerUsed(args.Performer, "DarkSwap");
+        }
+    }
+}
