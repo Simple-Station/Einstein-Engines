06d773ca7abdfac3df50f3d5880e7b05eb927e8e
Myra <vasilis@pikachu.systems>
2025-01-16 00:10:54 +0300

    Add a CCVar to allow from hiding admins in the reported player count. (#34406)
    Good for:
    - Keeping admins hidden
    - Not confuse players seeing 84/80 players
    
    Nicely pairs up with the ``admin.admins_count_for_max_players`` ccvar
    
    (cherry picked from commit 40fc4992e5e2fb6805d53ba0a665f05aa040949b)

--- Content.Server/GameTicking/GameTicker.StatusShell.cs
+++ Content.Server/GameTicking/GameTicker.StatusShell.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Text.Json.Nodes;
 using Content.Server.JoinQueue;
 using Content.Shared.CCVar;
@@ -46,7 +47,9 @@ namespace Content.Server.GameTicking
                 jObject["name"] = _baseServer.ServerName;
                 jObject["map"] = _gameMapManager.GetSelectedMap()?.MapName;
                 jObject["round_id"] = _gameTicker.RoundId;
-                jObject["players"] = _joinQueue.ActualPlayersCount;
+                jObject["players"] = _cfg.GetCVar(CCVars.AdminsCountInReportedPlayerCount)
+                    ? _joinQueue.ActualPlayersCount
+                    : _joinQueue.ActualPlayersCount - _adminManager.ActiveAdmins.Count();
                 jObject["soft_max_players"] = _cfg.GetCVar(CCVars.SoftMaxPlayers);
                 jObject["panic_bunker"] = _cfg.GetCVar(CCVars.PanicBunkerEnabled);
                 jObject["run_level"] = (int) _runLevel;
