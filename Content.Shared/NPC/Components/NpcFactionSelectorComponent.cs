e1b8bb5dee7e4dfd74fa0827c1fa857e2efcd040
Tim <timfalken@hotmail.com>
2025-01-20 22:18:23 +0100

    Remove redundant code from the component

--- Content.Shared/NPC/Components/NpcFactionSelectorComponent.cs
+++ Content.Shared/NPC/Components/NpcFactionSelectorComponent.cs
@@ -1,14 +1,11 @@
 using Content.Shared.NPC.Systems;
 using Robust.Shared.GameStates;
-using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.List;
 
 namespace Content.Shared.NPC.Components;
 
 [RegisterComponent, NetworkedComponent, Access(typeof(NpcFactionSelectorSystem))]
 public sealed partial class NpcFactionSelectorComponent : Component
 {
-    [DataField(customTypeSerializer: typeof(PrototypeIdListSerializer<EntityPrototype>))]
+    [DataField]
     public List<string> SelectableFactions = new();
 }
-
