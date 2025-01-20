513e57b745140d5643d802c4eb1a82f3caebbccd
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-01-19 22:36:07 -0400

    Multiple Central Commands & Ports (#175)
    <!--
    You can add an author after the `:cl:` to change the name that appears
    in the changelog (ex: `:cl: Death`)
    Leaving it blank will default to your GitHub display name
    This includes all available types for the changelog
    -->
    
    :cl:
    - add: Added a new central command map that is randomly picked alongside
    the old one (thank you to Spanky from Harmony)
    - add: Added Advanced SMES for mappers.
    - add: Added the atmospheric network monitor for seeing what the
    temperature, moles, and pressure is on every pipe everywhere through a
    computer.
    - add: Nukie med bundle now contains a compact defibrillator.
    - add: More mapping stuff.
    - add: Added the throngler plushie.
    - remove: Removed the Throngler as a possible loot spawn for gamble
    crates.

--- Content.Client/Mapping/MappingOverlay.cs
+++ Content.Client/Mapping/MappingOverlay.cs
@@ -1,9 +1,8 @@
-﻿using Robust.Client.GameObjects;
+﻿using System.Numerics;
+using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
-using Robust.Client.Input;
-using Robust.Client.Player;
-using Robust.Client.UserInterface;
 using Robust.Shared.Enums;
+using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
 using static Content.Client.Mapping.MappingState;
 
@@ -12,12 +11,7 @@ namespace Content.Client.Mapping;
 public sealed class MappingOverlay : Overlay
 {
     [Dependency] private readonly IEntityManager _entities = default!;
-    [Dependency] private readonly IPlayerManager _player = default!;
     [Dependency] private readonly IPrototypeManager _prototypes = default!;
-
-    // 1 off in case something else uses these colors since we use them to compare
-    private static readonly Color PickColor = new(1, 255, 0);
-    private static readonly Color DeleteColor = new(255, 1, 0);
 
     private readonly Dictionary<EntityUid, Color> _oldColors = new();
 
@@ -38,41 +32,82 @@ public sealed class MappingOverlay : Overlay
     {
         foreach (var (id, color) in _oldColors)
         {
-            if (!_entities.TryGetComponent(id, out SpriteComponent? sprite))
-                continue;
-
-            if (sprite.Color == DeleteColor || sprite.Color == PickColor)
+            if (_entities.TryGetComponent(id, out SpriteComponent? sprite))
                 sprite.Color = color;
         }
 
         _oldColors.Clear();
 
-        if (_player.LocalEntity == null)
-            return;
-
         var handle = args.WorldHandle;
         handle.UseShader(_shader);
 
-        switch (_state.State)
+        switch (_state.Meta.State)
         {
-            case CursorState.Pick:
+            case CursorState.Tile:
             {
-                if (_state.GetHoveredEntity() is { } entity &&
-                    _entities.TryGetComponent(entity, out SpriteComponent? sprite))
+                if (_state.GetHoveredTileBox2() is { } box)
+                    args.WorldHandle.DrawRect(box, _state.Meta.Color);
+
+                break;
+            }
+            case CursorState.Decal:
+            {
+                if (_state.GetHoveredDecalData() is { } hovered)
                 {
-                    _oldColors[entity] = sprite.Color;
-                    sprite.Color = PickColor;
+                    var (texture, box) = hovered;
+                    args.WorldHandle.DrawTextureRect(texture, box, _state.Meta.Color);
                 }
 
                 break;
             }
-            case CursorState.Delete:
+            case CursorState.Entity:
             {
                 if (_state.GetHoveredEntity() is { } entity &&
                     _entities.TryGetComponent(entity, out SpriteComponent? sprite))
                 {
                     _oldColors[entity] = sprite.Color;
-                    sprite.Color = DeleteColor;
+                    sprite.Color = _state.Meta.Color;
+                }
+
+                break;
+            }
+            case CursorState.Grid:
+            {
+                if (args.MapId == MapId.Nullspace || _state.GetHoveredGrid() is not { } grid)
+                    break;
+
+                var mapSystem = _entities.System<SharedMapSystem>();
+                var xformSystem = _entities.System<SharedTransformSystem>();
+
+                var tileSize = grid.Comp.TileSize;
+                var tileDimensions = new Vector2(tileSize, tileSize);
+                var (_, _, worldMatrix, invMatrix) = xformSystem.GetWorldPositionRotationMatrixWithInv(grid.Owner);
+                args.WorldHandle.SetTransform(worldMatrix);
+                var bounds = args.WorldBounds;
+                bounds = new Box2Rotated(bounds.Box.Enlarged(1), bounds.Rotation, bounds.Origin);
+                var localAABB = invMatrix.TransformBox(bounds);
+
+                var enumerator = mapSystem.GetLocalTilesEnumerator(grid.Owner, grid, localAABB);
+
+                while (enumerator.MoveNext(out var tileRef))
+                {
+                    var box = Box2.FromDimensions(tileRef.GridIndices, tileDimensions);
+                    args.WorldHandle.DrawRect(box, _state.Meta.Color);
+                }
+
+                break;
+            }
+            case CursorState.EntityOrTile:
+            {
+                if (_state.GetHoveredEntity() is { } entity &&
+                    _entities.TryGetComponent(entity, out SpriteComponent? sprite))
+                {
+                    _oldColors[entity] = sprite.Color;
+                    sprite.Color = _state.Meta.Color;
+                }
+                else if (_state.GetHoveredTileBox2() is { } box)
+                {
+                    args.WorldHandle.DrawRect(box, _state.Meta.SecondColor ?? _state.Meta.Color);
                 }
 
                 break;
