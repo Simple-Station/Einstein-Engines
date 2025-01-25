6bac235978a9fc166d777272d978940f789ab4e4
VMSolidus <evilexecutive@gmail.com>
2025-01-25 14:32:13 -0500

    Remove unused constants

--- Content.Shared/Weapons/Melee/SharedMeleeWeaponSystem.cs
+++ Content.Shared/Weapons/Melee/SharedMeleeWeaponSystem.cs
@@ -53,16 +53,6 @@ public abstract class SharedMeleeWeaponSystem : EntitySystem
 
     private const int AttackMask = (int) (CollisionGroup.MobMask | CollisionGroup.Opaque);
 
-    /// <summary>
-    /// Maximum amount of targets allowed for a wide-attack.
-    /// </summary>
-    public const int MaxTargets = 5;
-
-    /// <summary>
-    /// If an attack is released within this buffer it's assumed to be full damage.
-    /// </summary>
-    public const float GracePeriod = 0.05f;
-
     public override void Initialize()
     {
         base.Initialize();
