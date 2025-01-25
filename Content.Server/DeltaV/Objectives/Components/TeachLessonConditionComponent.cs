0f609bda6a2838ceb2e58263345a6ccf6aa063c1
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-01-25 16:10:27 -0400

    Update Content.Server/DeltaV/Objectives/Components/TeachLessonConditionComponent.cs
    Co-authored-by: VMSolidus <evilexecutive@gmail.com>
    Signed-off-by: sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>

--- Content.Server/DeltaV/Objectives/Components/TeachLessonConditionComponent.cs
+++ Content.Server/DeltaV/Objectives/Components/TeachLessonConditionComponent.cs
@@ -8,4 +8,13 @@ namespace Content.Server.DeltaV.Objectives.Components;
 /// Depends on <see cref="TargetObjectiveComponent"/> to function.
 /// </summary>
 [RegisterComponent, Access(typeof(TeachLessonConditionSystem))]
-public sealed partial class TeachLessonConditionComponent : Component;
+public sealed partial class TeachLessonConditionComponent : Component
+{
+    /// <summary>
+    ///     How close the assassin must be to the person "Being given a lesson", to ensure that the kill is reasonably
+    ///     something that could be the assassin's doing. This way the objective isn't resolved by the target getting killed
+    ///     by a space tick while on expedition.
+    /// </summary>
+    [DataField]
+    public float MaxDistance = 30f;
+}
