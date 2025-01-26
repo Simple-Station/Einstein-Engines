f325d2e78cfb2a2c09466c3e08a8a818cfd6dfbb
VMSolidus <evilexecutive@gmail.com>
2025-01-25 19:48:32 -0500

    Better Teach Lesson Code (#1657)
    # Description
    
    Thanks to Router for pointing out we can actually reuse code that is
    apparently just always running exclusively on player characters. So it
    turns out that this game actually has a system for tracking who the
    killer is. We can skip enumeration outright if the killer was the one
    who downed the player. There's still BETTER ways to do this, like
    subscribing to KillReportedEvent instead of MobStateChanged, but I'm a
    bit too zonked out to pursue that right now.

--- Content.Server/DeltaV/Objectives/Systems/TeachLessonConditionSystem.cs
+++ Content.Server/DeltaV/Objectives/Systems/TeachLessonConditionSystem.cs
@@ -21,35 +21,40 @@ public sealed class TeachLessonConditionSystem : EntitySystem
         SubscribeLocalEvent<MobStateChangedEvent>(OnMobStateChanged);
     }
 
-    // TODO: subscribe by ref at some point in the future
     private void OnMobStateChanged(MobStateChangedEvent args)
     {
-        if (args.NewMobState != MobState.Dead)
+        if (args.NewMobState != MobState.Critical || args.OldMobState >= args.NewMobState
+            || !TryComp<MindContainerComponent>(args.Target, out var mc) || mc.OriginalMind is not { } mindId)
             return;
 
-        // Get the mind of the entity that just died (if it had one)
-        // Uses OriginalMind so if someone ghosts or otherwise loses control of a mob, you can still greentext
-        if (!TryComp<MindContainerComponent>(args.Target, out var mc) || mc.OriginalMind is not {} mindId)
+        // If the attacker actually has the objective, we can just skip any enumeration outright.
+        if (args.Origin is not null
+            && HasComp<TeachLessonConditionComponent>(args.Origin)
+            && TryComp<TargetObjectiveComponent>(args.Origin, out var targetComp)
+            && targetComp.Target == mindId)
+        {
+            _codeCondition.SetCompleted(args.Origin!.Value);
             return;
+        }
 
         // Get all TeachLessonConditionComponent entities
         var query = EntityQueryEnumerator<TeachLessonConditionComponent, TargetObjectiveComponent>();
 
-        while (query.MoveNext(out var uid, out var conditionComp, out var targetObjective))
+        while (query.MoveNext(out var ent, out var conditionComp, out var targetObjective))
         {
             // Check if this objective's target matches the entity that died
             if (targetObjective.Target != mindId)
                 continue;
 
-            var userWorldPos = _transform.GetWorldPosition(uid);
+            var userWorldPos = _transform.GetWorldPosition(ent);
             var targetWorldPos = _transform.GetWorldPosition(args.Target);
-            
+
             var distance = (userWorldPos - targetWorldPos).Length();
             if (distance > conditionComp.MaxDistance
-                || Transform(uid).MapID != Transform(args.Target).MapID)
+                || Transform(ent).MapID != Transform(args.Target).MapID)
                 continue;
 
-            _codeCondition.SetCompleted(uid);
+            _codeCondition.SetCompleted(ent);
         }
     }
 }
