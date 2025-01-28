02c020bb765b8594a808fbd34d8652d05bd6845e
Angelo Fallaria <ba.fallaria@gmail.com>
2025-01-28 09:32:05 +0800

    fix random input bug in CloningSystem.Utility

--- Content.Server/Cloning/CloningSystem.Utility.cs
+++ Content.Server/Cloning/CloningSystem.Utility.cs
@@ -190,7 +190,7 @@ public sealed partial class CloningSystem
         }
 
         if (!HasComp<EmaggedComponent>(uid))
-            _material.SpawnMultipleFromMaterial(_random.Next(1, (int) (clonePod.UsedBiomass / 2.5)), clonePod.RequiredMaterial, Transform(uid).Coordinates);
+            _material.SpawnMultipleFromMaterial(_random.Next(1, Math.Max(1, (int) (clonePod.UsedBiomass / 2.5))), clonePod.RequiredMaterial, Transform(uid).Coordinates);
 
         clonePod.UsedBiomass = 0;
         clonePod.ActivelyCloning = false;
