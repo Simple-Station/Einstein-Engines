1aed964427039686939757d5a51fe4139514e3dc
Eris <eris@erisws.com>
2025-01-30 18:56:58 -0500

    It's called Fatal Laziness

---
+++ Content.Shared/_Arcadis/Scrap/ScrapReprocessorComponent.cs
@@ -0,0 +1,15 @@
+using Content.Shared.Random;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Arcadis.Scrap;
+
+/// <summary>
+/// Needa melt all the scrap from the mining.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ScrapReprocessorComponent : Component
+{
+
+}
