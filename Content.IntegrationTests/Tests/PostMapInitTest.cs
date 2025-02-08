451b89e8e1fb5db0b4ab71a2a5278cfbb98d26e1
Jonathan <44554691+Mike32oz@users.noreply.github.com>
2025-02-08 02:37:51 -0600

    Northway Ship For EE (MVP) (#1743)
    # Description
    Howdy, y'all. I'd like to give special thanks to Qulibly, who originally
    designed this shuttle. I'm posting an update to the Northway For EE
    here: As of right now, I can't get FTL working, but everything else
    works perfectly.
    
    Northway low pop 25
    
    I may make a station map for shipping ports for big ship expeditions.
    
    
    
    
    <details><summary><h1>Media</h1></summary>
    <p>
    
    
    ![Northway-0](https://github.com/user-attachments/assets/7489f94d-31b6-4f8a-9c21-325d98f446fa)
    
    
    </p>
    </details>
    
    ---
    
    # Changelog
    
    
    :cl: Mike32oz, Qulibly
    - add: Northway Map rotation pool.
    - tweak: Northway For EE.
    - remove: The pirate plundered her FTL Drive, but Northway's wings may
    be clipped, but she is still flyable.

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -70,7 +70,8 @@ namespace Content.IntegrationTests.Tests
             "Europa",
             "Meta",
             "Box",
-            "Bagel"
+            "Bagel",
+            "Northway"
         };
 
         /// <summary>
