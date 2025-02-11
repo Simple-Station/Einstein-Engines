817a0b24453e00349bd878029718f74022eb2fe2
SpaceManiac <tad@platymuus.com>
2025-02-11 12:59:50 +0300

    Fix 38 non-obsolete warnings (#33794)
    (cherry picked from commit 41223107356553ccbc7ba4f8b95e4f9049873d09)

--- Content.Shared/GameTicking/SharedGameTicker.cs
+++ Content.Shared/GameTicking/SharedGameTicker.cs
@@ -42,7 +42,10 @@ namespace Content.Shared.GameTicking
 
         private void OnRecordingStart(MappingDataNode metadata, List<object> events)
         {
-            metadata["roundId"] = new ValueDataNode(RoundId.ToString());
+            if (RoundId != 0)
+            {
+                metadata["roundId"] = new ValueDataNode(RoundId.ToString());
+            }
         }
 
         public TimeSpan RoundDuration()
