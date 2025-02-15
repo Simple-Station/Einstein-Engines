47dc56c32d49f77d671815293e3fb33811b9b889
sleepyyapril <flyingkarii@gmail.com>
2025-02-15 13:04:57 -0400

    fix imports

--- Content.Client/Guidebook/Controls/GuidebookWindow.xaml.cs
+++ Content.Client/Guidebook/Controls/GuidebookWindow.xaml.cs
@@ -10,6 +10,7 @@ using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 using Robust.Client.UserInterface.XAML;
 using Robust.Shared.ContentPack;
+using Robust.Shared.Prototypes;
 
 namespace Content.Client.Guidebook.Controls;
 
