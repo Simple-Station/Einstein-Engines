7a1a0b7e83c98625b6a4cc662e45f1fdd9fe069a
sleepyyapril <flyingkarii@gmail.com>
2025-02-15 11:08:31 -0400

    it works just never use debug

--- Content.Client/Storage/StorageBoundUserInterface.cs
+++ Content.Client/Storage/StorageBoundUserInterface.cs
@@ -9,7 +9,6 @@ public sealed class StorageBoundUserInterface : BoundUserInterface
 {
     [Dependency] private readonly IEntityManager _entManager = default!;
 
-    [Obsolete] public override bool DeferredClose => false;
     private readonly StorageSystem _storage;
 
     public StorageBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
