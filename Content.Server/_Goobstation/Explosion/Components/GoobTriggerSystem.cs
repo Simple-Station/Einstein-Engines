69f2946da36fd423c107fe99b63084a703d02206
Rouge2t7 <81053047+Sarahon@users.noreply.github.com>
2025-02-15 18:54:39 +0000

    Alright, trying one more time (durk isn't home)
    Signed-off-by: Rouge2t7 <81053047+Sarahon@users.noreply.github.com>

--- Content.Server/_Goobstation/Explosion/Components/GoobTriggerSystem.cs
+++ Content.Server/_Goobstation/Explosion/Components/GoobTriggerSystem.cs
@@ -1,34 +1,35 @@
 using Content.Server.Explosion.Components;
 using Content.Server.Explosion.EntitySystems;
-using Robust.Shared.Map.Components;
+using System.Linq;
 
 namespace Content.Server._Goobstation.Explosion.EntitySystems;
 
 public sealed partial class GoobTriggerSystem : EntitySystem
 {
+    public interface INonDeletable;
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<DeleteParentOnTriggerComponent, TriggerEvent>(HandleDeleteParentTrigger);
-    }
+@@ -14,10 +16,20 @@ public override void Initialize()
 
     private void HandleDeleteParentTrigger(Entity<DeleteParentOnTriggerComponent> entity, ref TriggerEvent args)
     {
-         if (!TryComp<TransformComponent>(entity, out var xform))
+        if (!TryComp<TransformComponent>(entity, out var xform))
+        var uid = entity.Owner;
+
+        if (!TryComp<TransformComponent>(uid, out var xform))
             return;
 
         var parentUid = xform.ParentUid;
 
-        // Don't delete map
-        if (HasComp<MapComponent>(parentUid))
-            return;
-
-        // Prevent deleting Grids
-        if (TryComp<TransformComponent>(parentUid, out var parentXform) && parentXform.GridUid == parentUid)
+        // Check if the parent entity has any component that implements INonDeletable
+        if (EntityManager.GetComponents(parentUid).Any(c => c is INonDeletable))
             return;
 
         EntityManager.QueueDeleteEntity(xform.ParentUid);
+        EntityManager.QueueDeleteEntity(parentUid);
         args.Handled = true;
     }
+
 }
