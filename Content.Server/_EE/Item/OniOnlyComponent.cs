f4768c5f0ed47df3ff2b7b77b1c2609d22acc7a1
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2025-03-05 20:46:43 -1000

    Shitcode real.

--- Content.Server/_EE/Item/OniOnlyComponent.cs
+++ Content.Server/_EE/Item/OniOnlyComponent.cs
@@ -2,4 +2,8 @@
 //   Locks an item to only be pick-upable by onis.
 // </summary>
 [RegisterComponent]
-public sealed partial class OniOnlyComponent : Component;
+public sealed partial class OniOnlyComponent : Component
+{
+    [DataField]
+    public TimeSpan KnockdownDuration = TimeSpan.FromSeconds(2);
+}
--- Content.Server/_EE/Item/OniOnlySystem.cs
+++ Content.Server/_EE/Item/OniOnlySystem.cs
@@ -1,29 +1,18 @@
 using Content.Server.Abilities.Oni;
-using Content.Shared.Damage;
+using Content.Shared.Ghost;
 using Content.Shared.Hands.EntitySystems;
-using Content.Shared.Inventory.VirtualItem;
-using Content.Shared.Item;
 using Content.Shared.Popups;
-using Content.Shared.StatusEffect;
 using Content.Shared.Stunnable;
-using Content.Shared.Timing;
-using Content.Shared.Weapons.Melee.Components;
 using Content.Shared.Weapons.Melee.Events;
-using Content.Shared.Weapons.Ranged.Systems;
-using Content.Shared.Wieldable.Components;
-using Robust.Shared.Audio;
-using Robust.Shared.Audio.Systems;
-using Robust.Shared.Network;
-using Robust.Shared.Timing;
 
 
 namespace Content.Server._EE.Item;
 
 public sealed class OniOnlySystem : EntitySystem
 {
-    [Dependency] private readonly SharedAudioSystem _audioSystem = default!;
-    [Dependency] private readonly StatusEffectsSystem _statusEffects = default!;
     [Dependency] private readonly SharedStunSystem _stun = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] private readonly SharedHandsSystem _hands = default!;
 
     public override void Initialize()
     {
@@ -33,18 +22,24 @@ public sealed class OniOnlySystem : EntitySystem
 
     private void OnMeleeAttempt(EntityUid uid, OniOnlyComponent component, ref AttemptMeleeEvent args)
     {
-        if (TryComp<WieldableComponent>(uid, out var wieldable) &&
-            !wieldable.Wielded && !HasComp<OniComponent>(args.PlayerUid))
-        {
-            {
-                args.Message = Loc.GetString("wieldable-component-requires-fumble", ("item", uid));
-                var playSound = !_statusEffects.HasStatusEffect(args.PlayerUid, "KnockedDown");
-                _stun.TryKnockdown(args.PlayerUid, TimeSpan.FromSeconds(1.5f), true);
-                if (playSound)
-                    _audioSystem.PlayPredicted(new SoundPathSpecifier("/Audio/Effects/slip.ogg"), args.PlayerUid, args.PlayerUid);
-            }
+        bool CanUse(EntityUid? uid) => HasComp<OniComponent>(uid) || HasComp<GhostComponent>(uid);
 
-            args.Cancelled = true;
-        }
+        if (CanUse(args.PlayerUid))
+            return;
+
+        KnockdownAndDropItem(component, args.PlayerUid, Loc.GetString("cult-item-component-attack-fail"));
+
+        args.Cancelled = true;
+    }
+
+    private void KnockdownAndDropItem(OniOnlyComponent component, EntityUid user, string message, bool serverOnly = false)
+    {
+        if (serverOnly)
+            _popup.PopupEntity(message, component.Owner, user); // Yes this is obsolete. Yes this is shitcode.
+        else
+            _popup.PopupPredicted(message, component.Owner, user);
+
+        _stun.TryKnockdown(user, component.KnockdownDuration, true);
+        _hands.TryDrop(user);
     }
 }
