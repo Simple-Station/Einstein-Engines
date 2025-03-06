c001b2a8f8afa31a47c0f3d01e82d3b1ec7d6450
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2025-03-05 21:01:20 -1000

    Okay but real this time.

--- Content.Server/_EE/Item/OniOnlySystem.cs
+++ Content.Server/_EE/Item/OniOnlySystem.cs
@@ -1,6 +1,7 @@
 using Content.Server.Abilities.Oni;
 using Content.Shared.Ghost;
 using Content.Shared.Hands.EntitySystems;
+using Content.Shared.IdentityManagement;
 using Content.Shared.Popups;
 using Content.Shared.Stunnable;
 using Content.Shared.Weapons.Melee.Events;
@@ -11,7 +12,7 @@ namespace Content.Server._EE.Item;
 public sealed class OniOnlySystem : EntitySystem
 {
     [Dependency] private readonly SharedStunSystem _stun = default!;
-    [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
     [Dependency] private readonly SharedHandsSystem _hands = default!;
 
     public override void Initialize()
@@ -27,18 +28,17 @@ public sealed class OniOnlySystem : EntitySystem
         if (CanUse(args.PlayerUid))
             return;
 
-        KnockdownAndDropItem(component, args.PlayerUid, Loc.GetString("oni-only-component-attack-fail"));
+        KnockdownAndDropItem(component, args.PlayerUid);
+
+        var selfMessage = Loc.GetString("oni-only-component-attack-fail-self", ("item", uid));
+        var othersMessage = Loc.GetString("oni-only-component-attack-fail-other", ("user", Identity.Entity(args.PlayerUid, EntityManager)), ("item", uid));
+        _popupSystem.PopupPredicted(selfMessage, othersMessage, args.PlayerUid, args.PlayerUid);
 
         args.Cancelled = true;
     }
 
-    private void KnockdownAndDropItem(OniOnlyComponent component, EntityUid user, string message, bool serverOnly = false)
+    private void KnockdownAndDropItem(OniOnlyComponent component, EntityUid user)
     {
-        if (serverOnly)
-            _popup.PopupEntity(message, user, user);
-        else
-            _popup.PopupPredicted(message, user, user);
-
         _stun.TryKnockdown(user, component.KnockdownDuration, true);
         _hands.TryDrop(user);
     }
--- Resources/Locale/en-US/_EE/popups/oni.ftl
+++ Resources/Locale/en-US/_EE/popups/oni.ftl
@@ -1,1 +1,2 @@
-oni-only-component-attack-fail = You can't quite get a grip on the item, it was seemingly designed for larger hands.
+oni-only-component-attack-fail-self = You can't quite get a grip on { CAPITALIZE(THE($item))}, it was seemingly designed for larger hands.
+oni-only-component-attack-fail-other = { CAPITALIZE(THE($item))} slips out of their hand. It's clearly too big for them.
