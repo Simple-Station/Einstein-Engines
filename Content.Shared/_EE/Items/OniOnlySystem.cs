0b10cf50bc0e86b04ad7b2cedebd82419c3c954d
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2025-03-05 22:13:12 -1000

    WHAT

--- Content.Shared/_EE/Items/OniOnlySystem.cs
+++ Content.Shared/_EE/Items/OniOnlySystem.cs
@@ -46,7 +46,7 @@ public sealed class OniOnlySystem : EntitySystem
             _audioSystem.PlayPredicted(new SoundPathSpecifier("/Audio/Effects/slip.ogg"), args.PlayerUid, args.PlayerUid);
 
         // Display the message to the player and cancel the melee attempt.
-        _popupSystem.PopupClient(args.Message, args.PlayerUid);
+        _popupSystem.PopupClient(args.Message, uid);
         args.Cancelled = true;
     }
 }
