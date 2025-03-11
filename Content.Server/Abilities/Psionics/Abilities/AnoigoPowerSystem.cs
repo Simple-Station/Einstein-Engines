648cbdec7954157c749ce0434f5e6a605b78b6bd
dootythefrooty <awhunter8@gmail.com>
2025-03-10 22:27:30 -0700

    first

---
+++ Content.Server/Abilities/Psionics/Abilities/AnoigoPowerSystem.cs
@@ -0,0 +1,33 @@
+﻿using Content.Shared.Abilities.Psionics;
+using Content.Shared.Actions.Events;
+using Content.Shared.Psionics.Glimmer;
+
+namespace Content.Server.Abilities.Psionics;
+
+
+public sealed class AnoigoPowerSystem : EntitySystem
+{
+    [Dependency] private readonly GlimmerSystem _glimmer = default!;
+    [Dependency] private readonly SharedPsionicAbilitiesSystem _psionics = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<PsionicComponent, AnoigoPowerActionEvent>(OnPowerUsed);
+    }
+
+    private void OnPowerUsed(EntityUid uid,PsionicComponent component, AnoigoPowerActionEvent args)
+    {
+        if (!_psionics.OnAttemptPowerUse(args.Performer, "anoigo"))
+            return;
+
+        Anoigo(args.Performer, args.Target);
+        _psionics.LogPowerUsed(args.Performer, "anoigo");
+        args.Handled = true;
+    }
+    private void Anoigo(EntityUid Performer, EntityUid Target)
+    {
+
+    }
+}
