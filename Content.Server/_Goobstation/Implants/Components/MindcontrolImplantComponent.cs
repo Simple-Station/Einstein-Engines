45ff18902347bcf1a71a0528c7c0570e37e88a96
EctoplasmIsGood <109397347+EctoplasmIsGood@users.noreply.github.com>
2025-03-12 21:48:46 -0500

    Delete Content.Server/_Goobstation/Implants/Components/MindcontrolImplantComponent.cs
    Signed-off-by: EctoplasmIsGood <109397347+EctoplasmIsGood@users.noreply.github.com>

--- Content.Server/_Goobstation/Implants/Components/MindcontrolImplantComponent.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Server.Implants.Components;
-
-[RegisterComponent]
-public sealed partial class MindcontrolImplantComponent : Component
-{
-    [DataField] public EntityUid? HolderUid = null; //who holds the implanter
-    [DataField] public EntityUid? ImplanterUid = null; // the implanter carrying the implant
-}
