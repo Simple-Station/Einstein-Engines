fd9cfbb512e5ff6130ecded09ee498224e29ed2d
EctoplasmIsGood <109397347+EctoplasmIsGood@users.noreply.github.com>
2025-03-17 12:19:30 -0500

    Lathe Messages (#1940)
    <!--
    This is a semi-strict format, you can add/remove sections as needed but
    the order/format should be kept the same
    Remove these comments before submitting
    -->
    
    # Description
    
    <!--
    Explain this PR in as much detail as applicable
    
    Some example prompts to consider:
    How might this affect the game? The codebase?
    What might be some alternatives to this?
    How/Who does this benefit/hurt [the game/codebase]?
    -->
    
    Adds lathe messages when new recipes are unlocked, ported from goob LRP
    
    
    # Changelog
    
    <!--
    You can add an author after the `:cl:` to change the name that appears
    in the changelog (ex: `:cl: Death`)
    Leaving it blank will default to your GitHub display name
    This includes all available types for the changelog
    -->
    
    :cl:
    - add: Lathes will now notify you when they have new recipes
    
    ---------
    
    Signed-off-by: VMSolidus <evilexecutive@gmail.com>
    Signed-off-by: EctoplasmIsGood <109397347+EctoplasmIsGood@users.noreply.github.com>
    Co-authored-by: BombasterDS <115770678+BombasterDS@users.noreply.github.com>
    Co-authored-by: VMSolidus <evilexecutive@gmail.com>

--- Content.Shared/Research/Components/TechnologyDatabaseComponent.cs
+++ Content.Shared/Research/Components/TechnologyDatabaseComponent.cs
@@ -2,6 +2,7 @@ using Content.Shared.Lathe;
 using Content.Shared.Research.Prototypes;
 using Content.Shared.Research.Systems;
 using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.List;
 
@@ -57,4 +58,12 @@ public sealed partial class TechnologyDatabaseComponent : Component
 /// server to all of it's clients.
 /// </remarks>
 [ByRefEvent]
-public readonly record struct TechnologyDatabaseModifiedEvent;
+public readonly record struct TechnologyDatabaseModifiedEvent // Goobstation - Lathe message on recipes update
+{
+    public readonly List<ProtoId<LatheRecipePrototype>> UnlockedRecipes;
+
+    public TechnologyDatabaseModifiedEvent(List<ProtoId<LatheRecipePrototype>>? unlockedRecipes = null)
+    {
+        UnlockedRecipes = unlockedRecipes ?? new();
+    }
+};
