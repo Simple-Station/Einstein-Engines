e27e0cc4cc1a0f18506d115f286c6be31744c896
VMSolidus <evilexecutive@gmail.com>
2025-03-18 00:09:15 -0400

    aaaaaa

--- Content.Shared/_Goobstation/Weapons/UseDelayBlockShoot/UseDelayBlockShootComponent.cs
+++
@@ -1,8 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared._Goobstation.Weapons.UseDelayBlockShoot;
-
-[RegisterComponent, NetworkedComponent]
-public sealed partial class UseDelayBlockShootComponent : Component
-{
-}
