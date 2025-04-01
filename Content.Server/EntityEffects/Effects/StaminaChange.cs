27c95c280679bab60008b5f03df2f1a461570e75
GNUtopia <erainach14@gmail.com>
2025-03-31 19:44:25 -0500

    a

--- Content.Server/EntityEffects/Effects/StaminaChange.cs
+++ Content.Server/EntityEffects/Effects/StaminaChange.cs
@@ -1,27 +1,28 @@
-using Content.Shared.Damage;
+using Content.Shared.Damage.Systems;
 using Content.Shared.EntityEffects;
-using Robust.Shared.Prototypes; /// probably don't need all this
+using Robust.Shared.Prototypes;
 
-namespace Content.Server.EntityEffects.Effects
+namespace Content.Server.EntityEffects.Effects;
+
+///<summary>
+///    Lets a chem heal/deal stamina damage.
+///</summary>
+public sealed partial class StaminaChange : EntityEffect
 {
-    ///<summary>
-    ///    Lets a chem heal/deal stamina damage.
-    ///</summary>
-    public sealed partial class StaminaChange : EntityEffect
-    {
-        /// <summary>
-        /// Stamina damage to apply every cycle.
-        ///Positive is damage, negative is healing, like health changes.
-        /// </summary>
-        [DataField]
-        public float Amount = 1.0f;
-
-        public override void Effect(EntityEffectBaseArgs args)
-        {
-            _stamina.TakeStaminaDamage(uid, Amount, visual: false);
-        }
-    }
+    [Dependency] private readonly StaminaSystem _stamina = default!;
     protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
         => Loc.GetString("reagent-effect-guidebook-stamina-change", ("chance", Probability),
             ("deltasign", MathF.Sign(Amount)));
+
+    ///<summary>
+    ///     Stamina damage to apply every cycle.
+    ///     Positive is damage, negative is healing, like health changes.
+    ///</summary>
+    [DataField("amount")]
+    public float Amount = 1.0f;
+
+    public override void Effect(EntityEffectBaseArgs args)
+    {
+        args.EntityManager.System<StaminaSystem>().TakeStaminaDamage(args.TargetEntity, Amount, visual: false);
+    }
 }
