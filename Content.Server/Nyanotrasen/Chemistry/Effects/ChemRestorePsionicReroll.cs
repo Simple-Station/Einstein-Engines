281f3c8b091d4536c5d10c462e48cce11bb4cd69
Rocket <rocketboss360@gmail.com>
2025-04-06 02:11:59 -0700

    localization nonsense

--- Content.Server/Nyanotrasen/Chemistry/Effects/ChemRestorePsionicReroll.cs
+++ Content.Server/Nyanotrasen/Chemistry/Effects/ChemRestorePsionicReroll.cs
@@ -11,7 +11,7 @@ namespace Content.Server.Chemistry.ReagentEffects
     public sealed partial class ChemRestorePsionicReroll : EntityEffect
     {
         protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
-            => Loc.GetString("reagent-effect-guidebook-chem-reroll-psionic", ("chance", Probability));
+            => Loc.GetString("reagent-effect-guidebook-chem-restorereroll-psionic");
 
         public override void Effect(EntityEffectBaseArgs args)
         {
