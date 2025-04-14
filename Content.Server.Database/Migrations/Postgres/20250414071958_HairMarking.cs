2e4fda298d9bdcaa882e65266a962b9a56f47552
WarMechanic <69510347+WarMechanic@users.noreply.github.com>
2025-04-14 20:59:01 +1000

    migration

---
+++ Content.Server.Database/Migrations/Postgres/20250414071958_HairMarking.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class HairMarking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE your_table_name
+                SET markings = jsonb_set(
+                    COALESCE(markings, '{}'::jsonb),
+                    '{hair_attributes}',
+                    COALESCE(markings->'hair_attributes', '[]'::jsonb) ||
+                    to_jsonb(hair_name || '@' || hair_colour)
+                )
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            /*
+                Nope, theres no reverting this!
+                Because the markings json doesn't specify slot in its format (nor does it need to), we cannot roll this change back.
+                At the moment, we don't drop the original hair table in case we do want to rollback
+            */
+        }
+    }
+}
---
+++ Content.Server.Database/Migrations/Sqlite/20250414071948_HairMarking.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class HairMarking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE your_table_name
+                SET markings = json_insert(
+                    COALESCE(markings, '{}'),
+                    '$.hairStyles',
+                    hair_name || '@' || hair_colour)
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            /*
+                Nope, theres no reverting this!
+                Because the markings json doesn't specify slot in its format (nor does it need to), we cannot roll this change back.
+                At the moment, we don't drop the original hair table in case we do want to rollback
+            */
+        }
+    }
+}
