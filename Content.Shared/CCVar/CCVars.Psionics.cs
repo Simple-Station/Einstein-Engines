b6e29b2c2faf530d1f0f69085d91d78910f87227
Your Name <EctoplasmIsGood@users.noreply.github.com>
2025-04-17 17:24:54 -0500

    Psychohistorian

--- Content.Shared/CCVar/CCVars.Psionics.cs
+++ Content.Shared/CCVar/CCVars.Psionics.cs
@@ -35,8 +35,10 @@ public sealed partial class CCVars
     ///     Congratulations, now they *actually* aren't a person anymore.
     ///     For people who complained that it wasn't obvious enough from the text that Mindbreaking is a form of Murder.
     /// </summary>
-    public static readonly CVarDef<bool> ScarierMindbreaking =
-        CVarDef.Create("psionics.scarier_mindbreaking", false, CVar.SERVERONLY);
+
+    /// This shit is evil and cannot be given to Durk
+    /// public static readonly CVarDef<bool> ScarierMindbreaking =
+    ///    CVarDef.Create("psionics.scarier_mindbreaking", false, CVar.SERVERONLY);
 
     /// <summary>
     /// Allow Ethereal Ent to PassThrough Walls/Objects while in Ethereal.
