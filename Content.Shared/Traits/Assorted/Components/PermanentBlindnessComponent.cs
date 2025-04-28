0187c38b3a96220f133281adf311cf259b3f4e50
GNUtopia <93669372+GNUtopia@users.noreply.github.com>
2025-04-28 13:46:20 -0500

    Make Disabilities Permanent + Glasses' Fury (#2327)
    # Description
    
    Removes the ability to circumvent the effects of Blindness and
    Wheelchair User through surgery. Additionally tweaks glasses to better
    counteract nearsightedness and minor eye damage. Most glasses now
    mitigate 3 points of eye damage, fully countering the nearsighted
    penalty, while jamjars mitigate 3.5 points of eye damage. Also tweaks
    the starting amounts of eye damage given by Blindness and Nearsighted.
    Blindness gives 8 points of eye damage, up from 6 (this doesn't have any
    visual effect but makes glasses worse since visual effects cap at 6
    points), and Nearsighted gives 3 points of eye damage, down from 4.
    
    ---
    <details><summary><h1>Media</h1></summary>
    <p>
    
    ![Updated Wheelchair User
    description](https://github.com/user-attachments/assets/5c97be58-1c07-452e-acaf-9f83290204ed)
    ![Updated Blindness
    description](https://github.com/user-attachments/assets/085676fc-ff25-47fc-a673-fb89d1899a38)
    ![Blindness being retained after
    surgery](https://github.com/user-attachments/assets/1aa6b35a-129e-4965-bf20-85f3c3f4bfa3)
    ![Inability to walk being retained after
    surgery](https://github.com/user-attachments/assets/2f3b9d19-a75f-429d-8c9a-eeb9ee7dd82b)
    (i'm trying to walk around after the surgery in the second video,
    prommy)
    ![Blindness being partially remedied by
    glasses](https://github.com/user-attachments/assets/14b8191d-d65f-4034-af37-ac331320dabe)
    ![Shortsighted being fully mitigated with
    glasses](https://github.com/user-attachments/assets/81236a50-1898-441e-bbb2-4ae78165fd99)
    
    </p>
    </details>
    
    ---
    
    # Changelog
    
    :cl:
    - tweak: Made Wheelchair User and Blindness permanent
    - tweak: Made Shortsighted less visually intense
    - tweak: Made glasses counteract eye damage better
    
    ---------
    
    Signed-off-by: Timfa <timfalken@hotmail.com>
    Co-authored-by: Timfa <timfalken@hotmail.com>

--- Content.Shared/Traits/Assorted/Components/PermanentBlindnessComponent.cs
+++ Content.Shared/Traits/Assorted/Components/PermanentBlindnessComponent.cs
@@ -10,5 +10,8 @@ public sealed partial class PermanentBlindnessComponent : Component
 {
     [ViewVariables(VVAccess.ReadWrite), DataField]
     public int Blindness = 0; // How damaged should their eyes be. Set 0 for maximum damage.
+
+    [ViewVariables(VVAccess.ReadWrite), DataField]
+    public bool Incurable = false; // Whether the permanent blindness can be fixed with surgery.
 }
 
