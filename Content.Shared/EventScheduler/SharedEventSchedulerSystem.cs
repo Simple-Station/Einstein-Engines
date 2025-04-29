67f7819f1415f3e59302d8f60b638f20cc215f48
WarMechanic <69510347+WarMechanic@users.noreply.github.com>
2025-04-29 16:35:31 +1000

    maybe this will work?

--- Content.Shared/EventScheduler/SharedEventSchedulerSystem.cs
+++ Content.Shared/EventScheduler/SharedEventSchedulerSystem.cs
@@ -32,9 +32,42 @@ public abstract partial class SharedEventSchedulerSystem : EntitySystem
     public override void Update(float frameTime)
     {
         base.Update(frameTime);
+
+        /*
+            huh?? weren't you supposed to make an optimised system to replace frameTime?
+            -   no, this replaces EQE, uses frametime to stay in sync with game tick
+                but it should only iterate 1/frame if no events occur on the frame
+        */
+        while (true)
+        {
+            // mostly a getter for values we're dealing with, if we can't get them for whatever reason something is wrong
+            if (!_eventQueue.TryPeek(out var index, out var time)
+                || !_eventList.TryGetValue(index, out var current))
+                break;
+
+            // if the pointed event has been cancelled, get the next event
+            if (current.Cancelled)
+            {
+                _eventQueue.Dequeue();
+                continue;
+            }
+
+            // if the pointed event can be triggered, raise it and get the next event
+            // this is in case >1 event is raised at the same time, allowing them to trigger on the same frame
+            if (_gameTiming.CurTime >= time)
+            {
+                _eventQueue.Dequeue();
+                RaiseLocalEvent(current.Uid, current.EventArgs);
+                continue;
+            }
+
+            // exit loop if nothing happens
+            break;
+        }
     }
 }
 
+// comparator function for priority queue, prefers the element that will occur earlier
 public sealed class EventSchedulerComparer : IComparer<TimeSpan>
 {
     [Dependency] private readonly IGameTiming _gameTiming = default!;
