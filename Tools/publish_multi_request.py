9569540b4589933ba9667632cd502e38c2b37f40
Eris <eris@erisws.com>
2025-05-02 12:21:07 -0400

    Set CDN fork-id

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,7 +15,7 @@ RELEASE_DIR = "release"
 # Forks should change these to publish to their own infrastructure.
 #
 ROBUST_CDN_URL = "https://cdn.simplestation.org/"
-FORK_ID = "einstein-engines"
+FORK_ID = "hullree"
 
 def main():
     session = requests.Session()
