2c019c01a43b0ba1346dc03748d4f1fd1f685c10
Tirochora <leotabletdb@gmail.com>
2025-05-03 15:38:32 -0400

    made a basic customize button, mostly just adding this to make the draft

--- Content.Client/Lobby/UI/CharacterPickerButton.xaml.cs
+++ Content.Client/Lobby/UI/CharacterPickerButton.xaml.cs
@@ -62,6 +62,8 @@ public sealed partial class CharacterPickerButton : ContainerButton
         View.SetEntity(_previewDummy);
         DescriptionLabel.Text = description;
 
+        var profileEditorVisible = CustomizeButton.Pressed;
+
         ConfirmDeleteButton.OnPressed += _ =>
         {
             Parent?.RemoveChild(this);
