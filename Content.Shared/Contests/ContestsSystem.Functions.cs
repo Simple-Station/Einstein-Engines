e992284590962d69008489542c67214fff9a4560
VMSolidus <evilexecutive@gmail.com>
2025-05-23 16:52:00 -0400

    Create ContestsSystem.Functions.cs

---
+++ Content.Shared/Contests/ContestsSystem.Functions.cs
@@ -0,0 +1,151 @@
+namespace Content.Shared.Contests;
+
+[ImplicitDataDefinitionForInheritors]
+public abstract partial class ContestFunction
+{
+    [DataField]
+    public bool BypassClamp;
+
+    [DataField]
+    public float RangeFactor = 1f;
+
+    public abstract float ComparatorFunction(
+        EntityUid target,
+        IEntityManager entityManager);
+
+    public abstract float ComparatorFunction(
+        EntityUid performer,
+        EntityUid target,
+        IEntityManager entityManager);
+}
+
+[ImplicitDataDefinitionForInheritors]
+public abstract partial class MassContestSingle : ContestFunction
+{
+    [DataField]
+    public float AverageMass = 71f;
+
+    public override float ComparatorFunction(
+        EntityUid target,
+        IEntityManager entityManager)
+    {
+        var contestsSystem = entityManager.System<ContestsSystem>();
+        return contestsSystem.MassContest(target, BypassClamp, RangeFactor, AverageMass);
+    }
+}
+
+[ImplicitDataDefinitionForInheritors]
+public abstract partial class MassContestDouble : ContestFunction
+{
+    public override float ComparatorFunction(
+        EntityUid performer,
+        EntityUid target,
+        IEntityManager entityManager)
+    {
+        var contestsSystem = entityManager.System<ContestsSystem>();
+        return contestsSystem.MassContest(performer, target, BypassClamp, RangeFactor);
+    }
+}
+
+[ImplicitDataDefinitionForInheritors]
+public abstract partial class StaminaContestSingle : ContestFunction
+{
+    public override float ComparatorFunction(
+        EntityUid target,
+        IEntityManager entityManager)
+    {
+        var contestsSystem = entityManager.System<ContestsSystem>();
+        return contestsSystem.StaminaContest(target, BypassClamp, RangeFactor);
+    }
+}
+
+[ImplicitDataDefinitionForInheritors]
+public abstract partial class StaminaContestDouble : ContestFunction
+{
+    public override float ComparatorFunction(
+        EntityUid performer,
+        EntityUid target,
+        IEntityManager entityManager)
+    {
+        var contestsSystem = entityManager.System<ContestsSystem>();
+        return contestsSystem.StaminaContest(performer, target, BypassClamp, RangeFactor);
+    }
+}
+
+[ImplicitDataDefinitionForInheritors]
+public abstract partial class HealthContestSingle : ContestFunction
+{
+    public override float ComparatorFunction(
+        EntityUid target,
+        IEntityManager entityManager)
+    {
+        var contestsSystem = entityManager.System<ContestsSystem>();
+        return contestsSystem.HealthContest(target, BypassClamp, RangeFactor);
+    }
+}
+
+[ImplicitDataDefinitionForInheritors]
+public abstract partial class HealthContestDouble : ContestFunction
+{
+    public override float ComparatorFunction(
+        EntityUid performer,
+        EntityUid target,
+        IEntityManager entityManager)
+    {
+        var contestsSystem = entityManager.System<ContestsSystem>();
+        return contestsSystem.HealthContest(performer, target, BypassClamp, RangeFactor);
+    }
+}
+
+[ImplicitDataDefinitionForInheritors]
+public abstract partial class MindContestSingle : ContestFunction
+{
+    [DataField]
+    public float AveragePsionicPotential = 1.6f;
+
+    public override float ComparatorFunction(
+        EntityUid target,
+        IEntityManager entityManager)
+    {
+        var contestsSystem = entityManager.System<ContestsSystem>();
+        return contestsSystem.MindContest(target, BypassClamp, RangeFactor, AveragePsionicPotential);
+    }
+}
+
+[ImplicitDataDefinitionForInheritors]
+public abstract partial class MindContestDouble : ContestFunction
+{
+    public override float ComparatorFunction(
+        EntityUid performer,
+        EntityUid target,
+        IEntityManager entityManager)
+    {
+        var contestsSystem = entityManager.System<ContestsSystem>();
+        return contestsSystem.MindContest(performer, target, BypassClamp, RangeFactor);
+    }
+}
+
+[ImplicitDataDefinitionForInheritors]
+public abstract partial class MoodContestSingle : ContestFunction
+{
+    public override float ComparatorFunction(
+        EntityUid target,
+        IEntityManager entityManager)
+    {
+        var contestsSystem = entityManager.System<ContestsSystem>();
+        return contestsSystem.MoodContest(target, BypassClamp, RangeFactor);
+    }
+}
+
+[ImplicitDataDefinitionForInheritors]
+public abstract partial class MoodContestDouble : ContestFunction
+{
+    public override float ComparatorFunction(
+        EntityUid performer,
+        EntityUid target,
+        IEntityManager entityManager)
+    {
+        var contestsSystem = entityManager.System<ContestsSystem>();
+        return contestsSystem.MoodContest(performer, target, BypassClamp, RangeFactor);
+    }
+}
