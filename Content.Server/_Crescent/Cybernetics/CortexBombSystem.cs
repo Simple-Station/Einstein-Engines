fa3c2a5b811b5387308b0b15e8d9c4845716188d
Eris <eris@erisws.com>
2025-05-24 17:31:04 -0400

    test

--- Content.Server/_Crescent/Cybernetics/CortexBombSystem.cs
+++ Content.Server/_Crescent/Cybernetics/CortexBombSystem.cs
@@ -1,4 +1,9 @@
+using Content.Server.Body.Systems;
+using Content.Server.Explosion.EntitySystems;
 using Content.Shared.Actions;
+using Content.Shared.Coordinates;
+using Content.Shared.Gibbing.Systems;
+using Content.Shared.Inventory;
 
 namespace Content.Shared.Cybernetics
 {
@@ -6,6 +11,14 @@ namespace Content.Shared.Cybernetics
     {
 
         [Dependency] private readonly SharedActionsSystem _actions = default!;
+
+        [Dependency] private readonly GibbingSystem _gibbing = default!;
+
+        [Dependency] private readonly BodySystem _body = default!;
+
+        [Dependency] private readonly ExplosionSystem _explosion = default!;
+
+        [Dependency] private readonly InventorySystem _inventory = default!;
         public override void Initialize()
         {
             base.Initialize();
@@ -18,7 +31,15 @@ namespace Content.Shared.Cybernetics
 
         private void OnCortexBombActivated(EntityUid uid, CortexBombComponent component, CortexBombActivatedEvent args)
         {
-            throw new NotImplementedException("i'm lazy");
+
+            var items = _inventory.GetHandOrInventoryEntities(uid);
+            foreach (var item in items)
+            {
+                Del(item);
+            }
+            _body.GibBody(uid, true);
+
+            _explosion.QueueExplosion(uid, ExplosionSystem.DefaultExplosionPrototypeId, 40, 1, 2);
         }
 
         private void OnStartup(EntityUid uid, CortexBombComponent component, ComponentStartup args)
