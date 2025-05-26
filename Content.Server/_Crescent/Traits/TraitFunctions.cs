a364209a6d61a1f544912db1cf8618fa69fbb67b
Eris <eris@erisws.com>
2025-05-25 22:59:16 -0400

    One down.

--- Content.Server/_Crescent/Traits/TraitFunctions.cs
+++ Content.Server/_Crescent/Traits/TraitFunctions.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Robust.Shared.GameObjects;
+using Newtonsoft.Json;
 using Robust.Shared.Serialization.Manager.Attributes;
 using JetBrains.Annotations;
 
@@ -19,10 +20,12 @@ namespace Content.Shared.Traits
             IEntityManager entityManager,
             ISerializationManager serializationManager)
         {
-            entityManager.EnsureComponent<ExtendDescriptionComponent>(uid, out var descComp);
+            if (!entityManager.TryGetComponent<ExtendDescriptionComponent>(uid, out var descComp))
+                return;
+
             foreach (var descExtension in DescriptionExtensions)
             {
-                var toRemove = descComp.DescriptionList.FirstOrDefault(ext => ext.Equals(descExtension));
+                var toRemove = descComp.DescriptionList.FirstOrDefault(ext => JsonConvert.SerializeObject(descExtension) == JsonConvert.SerializeObject(ext)); // the worst hack I have ever written but I have to do this
                 if (toRemove != null)
                     descComp.DescriptionList.Remove(toRemove);
             }
