82bd6abbfd729483fd5ec6acca57a4dd36ba7d37
MLGTASTICa <61350382+MLGTASTICa@users.noreply.github.com>
2025-05-28 01:47:34 +0300

    Revert "Fix LateJoin exploit , Optimize LateJoin Job GUI/"

--- Content.Shared/Preferences/HullrotSelectedSlotUpdatedEvent.cs
+++
@@ -1,8 +0,0 @@
-using Robust.Shared.Player;
-
-
-namespace Content.Shared.Preferences;
-
-
-[ByRefEvent]
-public readonly record struct HullrotSelectedSlotUpdated(ICommonSession session, int slotIndex);
