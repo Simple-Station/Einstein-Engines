3b8e209d06194090081d5ecab8df8ca5edbfc379
sleepyyapril <flyingkarii@gmail.com>
2025-05-29 15:59:50 -0300

    maaow

--- Content.Server/SubFloor/SubFloorHideSystem.cs
+++ Content.Server/SubFloor/SubFloorHideSystem.cs
@@ -9,7 +9,57 @@ public sealed class SubFloorHideSystem : SharedSubFloorHideSystem
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<SubFloorHideComponent, UnanchorAttemptEvent>(OnUnanchorAttempt);
+        SubscribeNetworkEvent<ShowSubfloorRequestEvent>(OnShowSubfloor);
+        SubscribeLocalEvent<GetVisMaskEvent>(OnGetVisibility);
+
+        _player.PlayerStatusChanged += OnPlayerStatus;
+    }
+
+    private void OnPlayerStatus(object? sender, SessionStatusEventArgs e)
+    {
+        if (e.NewStatus == SessionStatus.Connected)
+            return;
+
+        _showFloors.Remove(e.Session);
+
+        if (e.Session.AttachedEntity != null)
+            _eye.RefreshVisibilityMask(e.Session.AttachedEntity.Value);
+    }
+
+    private void OnGetVisibility(ref GetVisMaskEvent ev)
+    {
+        if (!TryComp(ev.Entity, out ActorComponent? actor))
+            return;
+
+        if (_showFloors.Contains(actor.PlayerSession))
+        {
+            ev.VisibilityMask |= (int)VisibilityFlags.Subfloor;
+        }
+    }
+
+    private void OnShowSubfloor(ShowSubfloorRequestEvent ev, EntitySessionEventArgs args)
+    {
+        // TODO: Commands are a bit of an eh? for client-only but checking shared perms
+        var ent = args.SenderSession.AttachedEntity;
+
+        if (!TryComp(ent, out EyeComponent? eyeComp))
+            return;
+
+        if (ev.Value)
+        {
+            _showFloors.Add(args.SenderSession);
+        }
+        else
+        {
+            _showFloors.Remove(args.SenderSession);
+        }
+
+        _eye.RefreshVisibilityMask((ent.Value, eyeComp));
+
+        RaiseNetworkEvent(new ShowSubfloorRequestEvent()
+        {
+            Value = ev.Value,
+        }, args.SenderSession);
     }
 
     // private void OnAnchorAttempt(EntityUid uid, SubFloorHideComponent component, AnchorAttemptEvent args)
