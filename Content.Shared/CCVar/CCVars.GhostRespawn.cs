fa67517fbead8286bbb3ff16a7041d716d738271
point2 <doublea654@gmail.com>
2025-06-10 11:41:01 +0200

    changed respawn timer from 15->5 minutes.
    also added a clear comment on which timer to use

--- Content.Shared/CCVar/CCVars.GhostRespawn.cs
+++ Content.Shared/CCVar/CCVars.GhostRespawn.cs
@@ -4,12 +4,19 @@ namespace Content.Shared.CCVar;
 
 public sealed partial class CCVars
 {
+
+    // 
+    // THIS is the real respawn timer that gets used ingame. it's also in minutes
+    // 
     public static readonly CVarDef<float> GhostRespawnTime =
-        CVarDef.Create("ghost.respawn_time", 15f, CVar.SERVERONLY);
+        CVarDef.Create("ghost.respawn_time", 5f, CVar.SERVERONLY);
 
     public static readonly CVarDef<int> GhostRespawnMaxPlayers =
         CVarDef.Create("ghost.respawn_max_players", 40, CVar.SERVERONLY);
 
+
+
+    // this also does nothing
     public static readonly CVarDef<bool> GhostAllowSameCharacter =
         CVarDef.Create("ghost.allow_same_character", false, CVar.SERVERONLY);
 }
