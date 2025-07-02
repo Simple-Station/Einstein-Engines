2457c111c017585a3dc0988195f1259c77170ae2
Eris <eris@erisws.com>
2025-07-01 20:49:12 -0400

    s

--- Content.Server/Salvage/SalvageSystem.cs
+++ Content.Server/Salvage/SalvageSystem.cs
@@ -52,7 +52,7 @@ namespace Content.Server.Salvage
         [Dependency] private readonly DungeonSystem _dungeon = default!;
         [Dependency] private readonly GravitySystem _gravity = default!;
         [Dependency] private readonly LabelSystem _labelSystem = default!;
-        [Dependency] private readonly MapLoaderSystem _map = default!;
+        [Dependency] private readonly MapLoaderSystem _loader = default!;
         [Dependency] private readonly MetaDataSystem _metaData = default!;
         [Dependency] private readonly RadioSystem _radioSystem = default!;
         [Dependency] private readonly SharedAudioSystem _audio = default!;
