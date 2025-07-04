29725e3c0e3e6d9a22b5fb1cec797d0d110f48af
Eris <eris@erisws.com>
2025-07-04 00:31:46 -0400

    hell dime-

--- Content.IntegrationTests/Tests/SaveLoadSaveTest.cs
+++ Content.IntegrationTests/Tests/SaveLoadSaveTest.cs
@@ -88,7 +88,7 @@ namespace Content.IntegrationTests.Tests
             await pair.CleanReturnAsync();
         }
 
-        private const string TestMap = "Maps/TheDen/pebble.yml";
+        private const string TestMap = "Maps/pebble.yml";
 
         /// <summary>
         ///     Loads the default map, runs it for 5 ticks, then assert that it did not change.
