1c55a7fe5b18519d043d49f48798d5e94b3de8c7
VMSolidus <evilexecutive@gmail.com>
2025-07-30 17:27:58 -0400

    More fixing stuffs

--- Content.Shared/Guidebook/Components/GuideHelpComponent.cs
+++ Content.Shared/Guidebook/Components/GuideHelpComponent.cs
@@ -1,34 +1,31 @@
-using Content.Shared.Guidebook;
+using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 
-namespace Content.Client.Guidebook.Components;
+namespace Content.Shared.Guidebook.Components;
 
 /// <summary>
 /// This component stores a reference to a guidebook that contains information relevant to this entity.
 /// </summary>
-[RegisterComponent]
-[Access(typeof(GuidebookSystem))]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class GuideHelpComponent : Component
 {
     /// <summary>
     /// What guides to include show when opening the guidebook. The first entry will be used to select the currently
     /// selected guidebook.
     /// </summary>
-    [DataField(required: true)]
+    [DataField(required: true), AutoNetworkedField]
     public List<ProtoId<GuideEntryPrototype>> Guides = new();
 
     /// <summary>
     /// Whether or not to automatically include the children of the given guides.
     /// </summary>
-    [DataField("includeChildren")]
-    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public bool IncludeChildren = true;
 
     /// <summary>
     /// Whether or not to open the UI when interacting with the entity while on hand.
     /// Mostly intended for books
     /// </summary>
-    [DataField("openOnActivation")]
-    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public bool OpenOnActivation;
 }
