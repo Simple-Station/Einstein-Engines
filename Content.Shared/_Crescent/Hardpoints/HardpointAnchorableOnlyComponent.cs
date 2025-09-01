fe3995ea0cf734a0ddbe1659e7392881489910bf
MLGTASTICa <61350382+MLGTASTICa@users.noreply.github.com>
2025-09-02 00:57:53 +0300

    ada

--- Content.Shared/_Crescent/Hardpoints/HardpointAnchorableOnlyComponent.cs
+++ Content.Shared/_Crescent/Hardpoints/HardpointAnchorableOnlyComponent.cs
@@ -5,10 +5,10 @@ namespace Content.Shared._Crescent.Hardpoints;
 /// <summary>
 /// This is used for...
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class HardpointAnchorableOnlyComponent : Component
 {
-    [ViewVariables(VVAccess.ReadOnly)]
+    [ViewVariables(VVAccess.ReadOnly), AutoNetworkedField]
     public EntityUid? anchoredTo;
     [ViewVariables(VVAccess.ReadWrite), DataField("class")]
     public weaponTypes CompatibleTypes = weaponTypes.Ballistic;
