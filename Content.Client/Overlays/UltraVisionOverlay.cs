a3a10f733b8f41b4655142497e998e48ad62bd77
VMSolidus <evilexecutive@gmail.com>
2025-11-17 22:03:03 -0500

    CC-0 Harpies

--- Content.Client/Overlays/UltraVisionOverlay.cs
+++ Content.Client/Overlays/UltraVisionOverlay.cs
@@ -1,3 +1,4 @@
+// UltraVisionOverlay.cs by Tyler Chase Johnson (also known as VMSolidus) is marked CC0 1.0. To view a copy of this mark, visit https://creativecommons.org/publicdomain/zero/1.0/
 using System.Numerics;
 using Robust.Client.Graphics;
 using Robust.Client.Player;
