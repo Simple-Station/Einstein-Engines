770f8c806ad04573bf9a8ee34672e63ac8dd9c13
wabash1 <wabash1@gmail.com>
2025-12-01 11:53:57 -0500

    Render fixes, range changes

--- Content.Shared/Shuttles/Systems/SharedRadarConsoleSystem.cs
+++ Content.Shared/Shuttles/Systems/SharedRadarConsoleSystem.cs
@@ -4,7 +4,10 @@ namespace Content.Shared.Shuttles.Systems;
 
 public abstract class SharedRadarConsoleSystem : EntitySystem
 {
-    public const float DefaultMaxRange = 256f;
+    public const float DefaultMaxRange = 1256f;
+    ///Changed from 256f to 1256f, affects bullet ranges only. over 1024 because 1024 causes rendering to stop at edges.
+    ///1448 would be best but this is a less common situation so players will not encounter it.
+    ///reggie plans to do circle viewport this will work for now, at that time we can do 1024. --wabash
 
     protected virtual void UpdateState(EntityUid uid, RadarConsoleComponent component)
     {
