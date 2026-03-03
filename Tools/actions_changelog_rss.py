d3752dac2812f35686e0b2fed5d196f99b6ebc15
Aiden <28298836+Aidenkrz@users.noreply.github.com>
2026-03-02 22:51:21 -0600

    Update server settings and changelog details

--- Tools/actions_changelog_rss.py
+++ Tools/actions_changelog_rss.py
@@ -35,24 +35,24 @@ CHANGELOG_RSS_KEY = os.environ.get("CHANGELOG_RSS_KEY")
 
 # Change these to suit your server settings
 # https://docs.fabfile.org/en/stable/getting-started.html#run-commands-via-connections-and-run
-SSH_HOST = "moon.spacestation14.com"
+SSH_HOST = "136.243.32.120"
 SSH_USER = "changelog-rss"
 SSH_PORT = 22
-RSS_FILE = "changelog.xml"
-XSL_FILE = "stylesheet.xsl"
+RSS_FILE = "/var/www/html/changelog.xml"
+XSL_FILE = "/var/www/html/stylesheet.xsl"
 HOST_KEYS = [
-    "AAAAC3NzaC1lZDI1NTE5AAAAIOBpGO/Qc6X0YWuw7z+/WS/65+aewWI29oAyx+jJpCmh"
+    "AAAAC3NzaC1lZDI1NTE5AAAAIGpFfKfwiDlKdf3t8A+pwupschcRnQfBf9Mj/EbCGKxB"
 ]
 
 # RSS feed parameters, change these
-FEED_TITLE       = "Space Station 14 Changelog"
-FEED_LINK        = "https://github.com/space-wizards/space-station-14/"
-FEED_DESCRIPTION = "Changelog for the official Wizard's Den branch of Space Station 14."
+FEED_TITLE       = "Goob Station Changelog"
+FEED_LINK        = "https://github.com/Goob-Station/Goob-Station"
+FEED_DESCRIPTION = "Changelog for Goob Station, a fork of Space Station 14."
 FEED_LANGUAGE    = "en-US"
-FEED_GUID_PREFIX = "ss14-changelog-wizards-"
-FEED_URL         = "https://central.spacestation14.io/changelog.xml"
+FEED_GUID_PREFIX = "ss14-changelog-goob-"
+FEED_URL         = "https://rss.goobstation.com/changelog.xml"
 
-CHANGELOG_FILE = "Resources/Changelog/Changelog.yml"
+CHANGELOG_FILE = "Resources/Changelog/GoobChangelog.yml"
 
 TYPES_TO_EMOJI = {
     "Fix":    "🐛",
