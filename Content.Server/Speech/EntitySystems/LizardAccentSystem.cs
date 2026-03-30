dca788bf02442928727a1392f73295290075b5f1
firfaronde <223457997+firfaronde@users.noreply.github.com>
2026-03-31 01:19:50 +0500

    Add Corvax localization support to LizardAccentSystem
    Signed-off-by: firfaronde <223457997+firfaronde@users.noreply.github.com>

--- Content.Server/Speech/EntitySystems/LizardAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/LizardAccentSystem.cs
@@ -1,11 +1,6 @@
-// SPDX-FileCopyrightText: 2022 Alex Evgrashin <aevgrashin@yandex.ru>
-// SPDX-FileCopyrightText: 2024 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
-// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
 using System.Text.RegularExpressions;
 using Content.Server.Speech.Components;
+using Robust.Shared.Random;
 using Content.Shared.Speech;
 
 namespace Content.Server.Speech.EntitySystems;
@@ -17,6 +12,25 @@ public sealed class LizardAccentSystem : EntitySystem
     private static readonly Regex RegexInternalX = new(@"(\w)x");
     private static readonly Regex RegexLowerEndX = new(@"\bx([\-|r|R]|\b)");
     private static readonly Regex RegexUpperEndX = new(@"\bX([\-|r|R]|\b)");
+
+    // Corvax-Localization-Start
+    private static readonly Regex _regexLowerC = new Regex("с+");
+    private static readonly Regex _regexUpperC = new Regex("С+");
+    private static readonly Regex _regexLowerZ = new Regex("з+");
+    private static readonly Regex _regexUpperZ = new Regex("З+");
+    private static readonly Regex _regexLowerSh = new Regex("ш+");
+    private static readonly Regex _regexUpperSh = new Regex("Ш+");
+    private static readonly Regex _regexLowerCh = new Regex("ч+");
+    private static readonly Regex _regexUpperCh = new Regex("Ч+");
+    private static readonly List<string> _replacementsSs = new List<string> { "сс", "ссс" };
+    private static readonly List<string> _replacementsSsUpper = new List<string> { "СС", "ССС" };
+    private static readonly List<string> _replacementsSh = new List<string> { "шш", "шшш" };
+    private static readonly List<string> _replacementsShUpper = new List<string> { "ШШ", "ШШШ" };
+    private static readonly List<string> _replacementsCh = new List<string> { "щщ", "щщщ" };
+    private static readonly List<string> _replacementsChUpper = new List<string> { "ЩЩ", "ЩЩЩ" };
+    // Corvax-Localization-End
+
+    [Dependency] private readonly IRobustRandom _random = default!; // Corvax-Localization
 
     public override void Initialize()
     {
@@ -39,6 +53,16 @@ public sealed class LizardAccentSystem : EntitySystem
         // eckS
         message = RegexUpperEndX.Replace(message, "ECKS$1");
 
+        // Corvax-Localization-Start
+        message = _regexLowerC.Replace(message, _random.Pick(_replacementsSs));
+        message = _regexUpperC.Replace(message, _random.Pick(_replacementsSsUpper));
+        message = _regexLowerZ.Replace(message, _random.Pick(_replacementsSs));       // для "з+" используются те же замены, что и для "с+"
+        message = _regexUpperZ.Replace(message, _random.Pick(_replacementsSsUpper)); // для "З+" используются те же замены, что и для "С+"
+        message = _regexLowerSh.Replace(message, _random.Pick(_replacementsSh));
+        message = _regexUpperSh.Replace(message, _random.Pick(_replacementsShUpper));
+        message = _regexLowerCh.Replace(message, _random.Pick(_replacementsCh));
+        message = _regexUpperCh.Replace(message, _random.Pick(_replacementsChUpper));
+        // Corvax-Localization-End
         args.Message = message;
     }
 }
